14a9746792b5849986b0d488ed899d122801f29e
muni <efrei.muni@gmail.com>
2019-08-03 15:27:42 +0800

    refactor

--- leet-code/src/main/java/datastructure/recursion/LeastCommonMultiple.java
+++
@@ -1,9 +0,0 @@
-package datastructure.recursion;
-
-public class LeastCommonMultiple {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		boolean done = false;
-	}
-}
