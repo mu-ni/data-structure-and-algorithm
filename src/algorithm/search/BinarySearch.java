3b26c11034fd72614b6605e48c4965e0391012e9
muni <muni@4paradigm.com>
2019-08-05 21:02:01 +0800

    add bs

---
+++ src/algorithm/search/BinarySearch.java
@@ -0,0 +1,43 @@
+package algorithm.search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[]{1,2,3,4,5,6,7,8,9}, 3));
+        System.out.println(binarySearch(new int[]{1,2,3,4,5,6,7,8,9}, 13));
+    }
+
+//    public static int binarySearch(int[] arr, int target) {
+//        int low = 0;
+//        int high = arr.length-1;
+//        int mid;
+//
+//        while(low < high) {
+//            mid = (low + high)/2;
+//            if(arr[mid] < target) {
+//                low = mid+1;
+//            } else if (arr[mid] > target) {
+//                high = mid-1;
+//            } else {
+//                return mid;
+//            }
+//        }
+//        return -1;
+//    }
+
+    public static int binarySearch(int[] arr, int target) {
+        return binarySearch(arr, 0, arr.length-1, target);
+    }
+
+    public static int binarySearch(int[] arr, int low, int high, int target) {
+        if (low > high) return -1;
+
+        int mid = (low+high)/2;
+        if(arr[mid] < target) {
+            return binarySearch(arr, mid+1, high, target);
+        }
+        if(arr[mid] > target) {
+            return binarySearch(arr, low, mid-1, target);
+        }
+        return mid;
+    }
+}
