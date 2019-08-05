2989578b89607b8ca8aaa7bfeca402b9c79c8b9d
muni <muni@4paradigm.com>
2019-08-05 18:58:37 +0800

    add shell

--- src/algorithm/sortsearch/InsertionSort.java
+++ src/algorithm/sortsearch/InsertionSort.java
@@ -1,4 +1,23 @@
 package algorithm.sortsearch;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(insertion(new int[]{1,4,2,6,9,3,7,8,5})));
+    }
+
+    // time complexity O(n^2)
+    // space complexity O(1)
+    public static int[] insertion(int[] nums) {
+        for(int i=1; i<nums.length; i++) {
+            int tmp = nums[i];
+            while(i >=1 && tmp < nums[i-1]) {
+                nums[i] = nums[i-1];
+                i--;
+            }
+            nums[i] = tmp;
+        }
+        return nums;
+    }
 }
--- src/algorithm/sortsearch/ShellSort.java
+++ src/algorithm/sortsearch/ShellSort.java
@@ -1,4 +1,27 @@
 package algorithm.sortsearch;
 
+import java.util.Arrays;
+
 public class ShellSort {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(shell(new int[]{1,4,2,6,9,3,7,8,5})));
+    }
+
+    public static int[] shell(int[] nums) {
+        for(int gap = nums.length/2; gap>0; gap /= 2) {
+            for (int i = gap; i < nums.length; i += 1) {
+                insertI(nums, gap, i);
+            }
+        }
+        return nums;
+    }
+
+    public static void insertI(int[] nums, int gap, int i) {
+        int tmp = nums[i];
+        while (i >= gap && nums[i - gap] > tmp) {
+            nums[i] = nums[i - gap];
+            i -= gap;
+        }
+        nums[i] = tmp;
+    }
 }
