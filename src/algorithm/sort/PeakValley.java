945123a8ac7c2a320d3ac8b2b9fb509c9ed4c415
muni <muni@4paradigm.com>
2019-08-07 12:29:18 +0800

    add peakvalley2

--- src/algorithm/sort/PeakValley.java
+++ src/algorithm/sort/PeakValley.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class PeakValley {
     public static void main(String[] args) {
-        System.out.print(Arrays.toString(peakValley(new int[]{0,1,4,7,8,9})));
+        System.out.println(Arrays.toString(peakValley(new int[]{0,1,4,7,8,9})));
+        System.out.println(Arrays.toString(peakValley2(new int[]{0,1,4,7,8,9})));
     }
 
     // time complexity O(nlogn)
@@ -21,5 +22,28 @@ public class PeakValley {
         int tmp = arr[a];
         arr[a] = arr[b];
         arr[b] = tmp;
+    }
+
+    // time complexity O(n)
+    public static int[] peakValley2(int[] arr) {
+        for(int i=0; i<arr.length-1; i += 2) {
+            int maxIndex = maxIndex(arr, i-1, i, i+1);
+            if(arr[maxIndex] != arr[i]) {
+                swap(arr, maxIndex, i);
+            }
+        }
+        return arr;
+    }
+
+    private static int maxIndex(int[] arr, int a, int b, int c) {
+        int lastIndex = arr.length-1;
+        int A = a >= 0 && a <= lastIndex ? arr[a] : Integer.MIN_VALUE;
+        int B = b >= 0 && b <= lastIndex ? arr[b] : Integer.MIN_VALUE;
+        int C = c >= 0 && c <= lastIndex ? arr[c] : Integer.MIN_VALUE;
+
+        int max = Math.max(A, Math.max(B,C));
+        if(A == max) return a;
+        if(B == max) return b;
+        return c;
     }
 }
