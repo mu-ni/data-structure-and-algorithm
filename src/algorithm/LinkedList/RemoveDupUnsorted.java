2e12b29df8a6a1aeaf8ab2411e4b6bf31f58e779
muni <efrei.muni@gmail.com>
2019-08-08 23:22:19 +0800

    add m2

--- src/algorithm/LinkedList/RemoveDupUnsorted.java
+++ src/algorithm/LinkedList/RemoveDupUnsorted.java
@@ -2,6 +2,9 @@ package algorithm.LinkedList;
 
 import algorithm.LinkedList.Dao.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveDupUnsorted {
     public static void main(String[] args) {
         ListNode head = ListNode.getList(new int[]{1,4,2,2,3,1});
@@ -9,6 +12,22 @@ public class RemoveDupUnsorted {
     }
 
     public static ListNode removeDuplicates(ListNode head) {
+        Set<Integer> map = new HashSet<>();
+        map.add(head.data);
+
+        ListNode cur = head;
+        while(cur.next != null) {
+            if(map.contains(cur.next.data)) {
+                cur.next = cur.next.next;
+                continue;
+            }
+            map.add(cur.next.data);
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public static ListNode removeDuplicates2(ListNode head) {
         ListNode cur = head;
         while(cur != null) {
             ListNode pt = cur;
@@ -23,4 +42,5 @@ public class RemoveDupUnsorted {
         }
         return head;
     }
+
 }
