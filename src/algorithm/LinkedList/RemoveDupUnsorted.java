e2069bbdb584604c31120a113f66d053b04b0a7f
muni <muni@4paradigm.com>
2019-08-08 21:29:36 +0800

    add

--- src/algorithm/LinkedList/RemoveDupUnsorted.java
+++ src/algorithm/LinkedList/RemoveDupUnsorted.java
@@ -11,12 +11,13 @@ public class RemoveDupUnsorted {
     public static ListNode removeDuplicates(ListNode head) {
         ListNode cur = head;
         while(cur != null) {
+            ListNode prev = cur;
             ListNode cur2 = cur.next;
             while (cur2 != null) {
                 if(cur.data == cur2.data) {
-                    // rm cur2
-
+                    prev.next = cur2.next;
                 }
+                prev = cur2;
                 cur2 = cur2.next;
             }
             cur = cur.next;
