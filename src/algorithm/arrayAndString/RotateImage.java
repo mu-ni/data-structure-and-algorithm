02338a270c4733985915e6c7ab3212ac87621d0b
muni <muni@4paradigm.com>
2019-08-08 19:20:10 +0800

    add matrix

---
+++ src/algorithm/arrayAndString/RotateImage.java
@@ -0,0 +1,23 @@
+package algorithm.arrayAndString;
+
+import java.util.Arrays;
+
+public class RotateImage {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(rotate(new int[][]{{1,2,3},{4,5,6},{7,8,9}})));
+    }
+
+    public static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0; i<n/2; i++) {
+            for(int j=i; j<n-1-i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+        return matrix;
+    }
+}
---
+++ src/algorithm/arrayAndString/ZeroMatrix.java
@@ -0,0 +1,43 @@
+package algorithm.arrayAndString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ZeroMatrix {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(setZeroes(new int[][]{{1,1,1},{1,0,1},{1,1,1}})));
+    }
+
+    public static int[][] setZeroes(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        List<List<Integer>> lists = new ArrayList<>();
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                List<Integer> list = new ArrayList<>();
+                if(matrix[i][j] == 0) {
+                    list.add(i);
+                    list.add(j);
+                    lists.add(list);
+                }
+            }
+        }
+
+        for(List<Integer> list : lists) {
+            setZero(matrix, list.get(0), list.get(1));
+        }
+
+        return matrix;
+    }
+
+    public static void setZero(int[][] matrix, int a, int b) {
+        for(int i=0; i<matrix.length; i++) {
+            matrix[i][b] = 0;
+        }
+        for(int i=0; i<matrix[0].length; i++) {
+            matrix[a][i] = 0;
+        }
+    }
+}
