c87a4edee7f564da72a31ad7d6fa34dbb475dc9b
muni <efrei.muni@gmail.com>
2019-08-09 00:25:32 +0800

    update

--- src/algorithm/LinkedList/RemoveNthFromEnd.java
+++ src/algorithm/LinkedList/RemoveNthFromEnd.java
@@ -5,17 +5,24 @@ import algorithm.LinkedList.Dao.ListNode;
 public class RemoveNthFromEnd {
     public static void main(String[] args) {
         ListNode head = ListNode.getList(new int[]{1,2,3,4,5});
-        System.out.println(removeNthFromEnd(head, 3).toString());
+        System.out.println(removeNthFromEnd(head, 2).toString());
+
+        head = ListNode.getList(new int[]{1,2});
+        System.out.println(removeNthFromEnd(head, 2).toString());
+
+        head = ListNode.getList(new int[]{1});
+        System.out.println(removeNthFromEnd(head, 1));
     }
 
+    // Given n will always be valid.
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        ListNode cur = dummy;
-        ListNode nth = dummy;
+        ListNode cur = head;
+        ListNode nth = head;
 
         for(int i=0; i<=n; i++) {
+            if(nth == null) {//n == length, remove first
+                return head.next;
+            }
             nth = nth.next;
         }
 
@@ -25,6 +32,6 @@ public class RemoveNthFromEnd {
         }
 
         cur.next = cur.next.next;
-        return dummy.next;
+        return head;
     }
 }
