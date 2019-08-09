4096c3a01b44d3f6f5f8fa064dd282403d4ccb01
muni <muni@4paradigm.com>
2019-08-09 17:40:59 +0800

    intersection

---
+++ src/algorithm/LinkedList/Intersection.java
@@ -0,0 +1,42 @@
+package algorithm.LinkedList;
+
+import algorithm.LinkedList.Dao.ListNode;
+
+public class Intersection {
+    public static void main(String[] args) {
+        ListNode l1 = ListNode.getList(new int[]{1,2,3,4,5,6});
+        ListNode l2 = ListNode.getList(new int[]{7,8,9,10,4,5,6});
+        System.out.println(getIntersectionNode(l1, l2).toString());
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = len(headA);
+        int lenB = len(headB);
+
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return headA;
+    }
+
+    public static int len(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+}
