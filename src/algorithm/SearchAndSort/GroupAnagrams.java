80ffccf17df6710503f68d2766503f7071b03ddf
muni <efrei.muni@gmail.com>
2019-08-11 17:11:16 +0800

    group anagrams

---
+++ src/algorithm/SearchAndSort/GroupAnagrams.java
@@ -0,0 +1,26 @@
+package algorithm.SearchAndSort;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, ArrayList<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String sortedStr = new String(arr);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
