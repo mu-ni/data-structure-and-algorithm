ceb02c5367a3de625b0bab9cc59585d600fd7baa
muni <muni@4paradigm.com>
2019-08-12 14:45:40 +0800

    add O

--- src/algorithm/RecursionAndDP/ClimbingStairs.java
+++ src/algorithm/RecursionAndDP/ClimbingStairs.java
@@ -8,11 +8,17 @@ public class ClimbingStairs {
         System.out.println(climb4(25));
     }
 
+    // recursion
+    // time complexity O(2^n)
+    // space complexity O(n)
     public static int climb(int n) {
         if (n <= 2) return n;
         return climb(n-1) + climb(n-2);
     }
 
+    // DP Top Down
+    // time complexity O(n)
+    // space complexity O(n)
     static int[] arr = new int[128];
     public static int climb2(int n) {
         if (n <= 2) return n;
@@ -22,6 +28,9 @@ public class ClimbingStairs {
         return arr[n] = climb2(n-1) + climb2(n-2);
     }
 
+    // iteration
+    // time complexity O(n)
+    // space complexity O(1)
     public static int climb3(int n) {
         if (n <= 2) return n;
         int a = 1;
@@ -34,6 +43,9 @@ public class ClimbingStairs {
         return b;
     }
 
+    // DP Down Top
+    // time complexity O(n)
+    // space complexity O(n)
     public static int climb4(int n) {
         if (n <= 2) return n;
         int[] arr = new int[n+1];
