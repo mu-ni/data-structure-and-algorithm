ef8701045c2883b7c815df2dba2eeae0345dd1fd
muni <muni@4paradigm.com>
2019-08-12 11:27:41 +0800

    dfs

--- src/algorithm/TreeAndGraph/LevelOrder.java
+++ src/algorithm/TreeAndGraph/LevelOrder.java
@@ -10,19 +10,20 @@ import java.util.Queue;
 public class LevelOrder {
     public static void main(String[] args) {
         System.out.println(bfs(Tree.genTree()));
+        System.out.println(dfs(Tree.genTree()));
     }
     
     public static List<List<Integer>> bfs(Tree root) {
-        List<List<Integer>> rst = new LinkedList<>();
+        List<List<Integer>> rst = new ArrayList<>();
         if(root == null) return rst;
 
         Queue<Tree> queue = new LinkedList<>();
         queue.offer(root);
 
         while(!queue.isEmpty()) {
-            int level = queue.size();
+            int size = queue.size();
             List<Integer> list = new ArrayList<>();
-            for (int i=0; i<level; i++) {
+            for (int i=0; i<size; i++) {
                 Tree node = queue.poll();
                 list.add(node.val);
 
@@ -38,5 +39,23 @@ public class LevelOrder {
         return rst;
     }
 
-    // dfs
+    public static List<List<Integer>> dfs(Tree root) {
+        List<List<Integer>> rst = new ArrayList<>();
+        if (root == null) return rst;
+        return dfs(rst, root, 0);
+    }
+
+    public static List<List<Integer>> dfs(List<List<Integer>> rst, Tree node, int level) {
+        if (rst.size() < level+1) {
+            rst.add(new ArrayList<>());
+        }
+        rst.get(level).add(node.val);
+        if (node.left != null) {
+            dfs(rst, node.left, level+1);
+        }
+        if (node.right != null) {
+            dfs(rst, node.right, level+1);
+        }
+        return rst;
+    }
 }
