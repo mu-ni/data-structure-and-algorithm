04d121b7b3576828c10a94457ca53a1590198916
muni <muni@4paradigm.com>
2019-08-12 20:01:28 +0800

    grid

--- src/algorithm/RecursionAndDP/UniquePath.java
+++ src/algorithm/RecursionAndDP/UniquePath.java
@@ -2,11 +2,11 @@ package algorithm.RecursionAndDP;
 
 public class UniquePath {
     public static void main(String[] args) {
-        System.out.print(uniquePaths(4, 4));
+        System.out.println(uniquePaths(36, 7));
+        System.out.println(uniquePaths2(36, 7));
     }
 
     public static int uniquePaths(int m, int n) {
-        // write your code here
         int[][] grid = new int[m][n];
         for(int i=0; i< m; i++) {
             for(int j=0; j<n; j++) {
@@ -20,23 +20,16 @@ public class UniquePath {
         return grid[m-1][n-1];
     }
 
-//    public static int uniquePaths(int m, int n) {
-//        return dfs(m-1, n-1);
-//    }
-//    static int rst = 0;
-//    public static int dfs(int r, int c) {
-//        System.out.println(r + "," + c);
-//        if(r == 0 && c == 0) {
-//            rst++;
-//        }
-//        if(r > 0) {
-//            dfs(r-1, c);
-//        }
-//        if(c > 0) {
-//            dfs(r, c-1);
-//        }
-//        return rst;
-//    }
+    public static int uniquePaths2(int m, int n) {
+        return dfs(m, n);
+    }
 
+    public static int dfs(int r, int c) {
+        if(r == 1 || c == 1) {
+            return 1;
+        }
+
+        return dfs(r-1, c) + dfs(r, c-1);
+    }
 
 }
