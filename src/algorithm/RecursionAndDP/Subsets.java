65da83aa6f79f473384019b536823306a0bbcece
muni <muni@4paradigm.com>
2019-08-13 11:14:31 +0800

    subset

--- src/algorithm/RecursionAndDP/Subsets.java
+++ src/algorithm/RecursionAndDP/Subsets.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class Subsets {
     public static void main(String[] args) {
-        System.out.print(subsets(new int[]{1,2,3}));
+        System.out.println(subsets(new int[]{1,2,3}));
+        System.out.println(subsets2(new int[]{1,2,3}));
     }
 
     public static List<List<Integer>> subsets(int[] nums) {
@@ -24,19 +25,19 @@ public class Subsets {
     }
 
     // recursion
-//    public List<List<Integer>> subsets(int[] nums) {
-//        // Arrays.sort(nums);
-//        List<List<Integer>> rst = new ArrayList<>();
-//        backtrack(rst, nums, new ArrayList<>(), 0);
-//        return rst;
-//    }
-//
-//    public void dfs(List<List<Integer>> rst, int[] nums, List<Integer> path, int start) {
-//        rst.add(new ArrayList<>(path));
-//        for(int i=start; i< nums.length; i++) {
-//            path.add(nums[i]);
-//            backtrack(rst, nums, path, i+1);
-//            path.remove(path.size()-1);
-//        }
-//    }
+    public static List<List<Integer>> subsets2(int[] nums) {
+        // Arrays.sort(nums);
+        List<List<Integer>> rst = new ArrayList<>();
+        backtrack(rst, nums, new ArrayList<>(), 0);
+        return rst;
+    }
+
+    public static void backtrack(List<List<Integer>> rst, int[] nums, List<Integer> path, int start) {
+        rst.add(new ArrayList<>(path));
+        for(int i=start; i< nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(rst, nums, path, i+1);
+            path.remove(path.size()-1);
+        }
+    }
 }
