1b329778c5eb50193c6024274a662416c74977cb
muni <efrei.muni@gmail.com>
2019-08-13 23:46:24 +0800

    fill

--- src/algorithm/RecursionAndDP/CoinChange.java
+++ src/algorithm/RecursionAndDP/CoinChange.java
@@ -1,28 +1,41 @@
 package algorithm.RecursionAndDP;
 
-import com.sun.deploy.util.ArrayUtil;
-import com.sun.tools.javac.util.ArrayUtils;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class CoinChange {
     public static void main(String[] args) {
-//        System.out.println(coinChange(new int[]{1,2,5}, 11));
-        System.out.println(coinChange(new int[]{1,2,5}, 100));
+        System.out.println(coinChange(new int[]{1,2,5}, 30));
+        System.out.println(coinChange2(new int[]{1,2,5}, 30));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        return helper(coins, amount, new int[amount]);
+    }
+
+    public static int helper(int[] coins, int remain, int[] count) {
+        if (remain < 0) return -1;
+        if (remain == 0) return 0;
+        if(count[remain-1] != 0) return count[remain-1];
+
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<coins.length; i++) {
+            int res = helper(coins, remain - coins[i], count);
+            if (res >= 0 && res < min) {
+                min = res+1;
+            }
+        }
+        count[remain-1] = (min==Integer.MAX_VALUE) ? -1 : min;
+        return count[remain-1];
     }
 
     public static int rst = Integer.MAX_VALUE;
-    public static int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);// REVERSE
-        Collections.reverse(Arrays.asList(coins));
-        helper(coins, amount, new ArrayList<>());
+    public static int coinChange2(int[] coins, int amount) { // work but slow
+        helper2(coins, amount, new ArrayList<>());
         return rst == Integer.MAX_VALUE ? -1 : rst;
     }
 
-    public static void helper(int[] coins, int remain, List<Integer> path) {
+    public static void helper2(int[] coins, int remain, List<Integer> path) {
         if (remain < 0) return;
         if (remain == 0 && path.size() < rst) {
             rst = path.size();
@@ -30,7 +43,7 @@ public class CoinChange {
 
         for (int i=0; i<coins.length; i++) {
             path.add(coins[i]);
-            helper(coins, remain - coins[i], path);
+            helper2(coins, remain - coins[i], path);
             path.remove(path.size()-1);
         }
     }
