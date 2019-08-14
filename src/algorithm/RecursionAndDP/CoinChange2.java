b6659aeffe024f5ccb0c0985efe9463bf504bb21
muni <muni@4paradigm.com>
2019-08-14 14:15:34 +0800

    coin

--- src/algorithm/RecursionAndDP/CoinChange2.java
+++ src/algorithm/RecursionAndDP/CoinChange2.java
@@ -1,7 +1,31 @@
 package algorithm.RecursionAndDP;
 
 public class CoinChange2 { //combinations make up amount
+    public static void main(String[] args) {
+        System.out.println(coinChange(new int[]{1,2,3}, 5));
+        System.out.println(coinChange2(new int[]{1,2,3}, 6));
+    }
+
+    public static int coinChange(int[] coins, int amount) { // use repeated coins
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i=coin; i<=amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
 
 
-
+    public static int coinChange2(int[] coins, int amount) { // use non-repeated coins
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i=amount; i>=coin; i--) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
 }
