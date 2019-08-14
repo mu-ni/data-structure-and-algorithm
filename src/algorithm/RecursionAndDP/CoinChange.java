4926fc68f5f00bd02a7a1ee9f4007cb43649e617
muni <muni@4paradigm.com>
2019-08-14 13:58:48 +0800

    add method3

--- src/algorithm/RecursionAndDP/CoinChange.java
+++ src/algorithm/RecursionAndDP/CoinChange.java
@@ -5,39 +5,8 @@ import java.util.Arrays;
 public class CoinChange {
     public static void main(String[] args) {
         System.out.println(coinChange(new int[]{1,2,3}, 20));
-        System.out.println(coinChange2(new int[]{1,2,5}, 11));
-    }
-
-    public static int coinChange2(int[] coins, int amount) {
-        int[][] dp = new int[coins.length+1][amount+1];
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, Integer.MAX_VALUE));
-
-        for (int i = 0; i < coins.length; ++i){
-            dp[i][0] = 0;
-        }
-
-        for (int j = 1; j <= amount; ++j){
-            if (j >= coins[0]){
-                if (dp[0][j - coins[0]] == Integer.MAX_VALUE) {
-                    continue;
-                }
-//                int a = dp[0][j - coins[0]] + 1;
-//                int b = dp[0][j];
-                dp[0][j] = Math.min(dp[0][j - coins[0]] + 1, dp[0][j]);
-            }
-        }
-
-        for (int i=1; i<coins.length; ++i) {
-            int coin = coins[i];
-            for (int j=1; j<=amount; ++j) {
-                if (j < coin || dp[i][j-coin] == Integer.MAX_VALUE) {
-                    dp[i][j] = dp[i-1][j];
-                    continue;
-                }
-                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-coin]+1);
-            }
-        }
-        return dp[coins.length-1][amount] == Integer.MAX_VALUE ? -1 : dp[coins.length-1][amount];
+        System.out.println(coinChange2(new int[]{1,2,3}, 20));
+        System.out.println(coinChange3(new int[]{1,2,3}, 20));
     }
 
     public static int coinChange(int[] coins, int amount) {
@@ -58,6 +27,52 @@ public class CoinChange {
         }
         count[remain] = (min==Integer.MAX_VALUE) ? -1 : min;
         return count[remain];
+    }
+
+    public static int coinChange2(int[] coins, int amount) { // 2d array
+        int[][] dp = new int[coins.length][amount+1];
+        Arrays.stream(dp).forEach(a -> Arrays.fill(a, Integer.MAX_VALUE));
+
+        for (int i = 0; i < coins.length; i++){ // amount = 0
+            dp[i][0] = 0;
+        }
+
+        for (int j = 1; j <= amount; j++){ // only use coins[0]
+            if (j >= coins[0]){
+                if (dp[0][j - coins[0]] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                dp[0][j] = Math.min(dp[0][j - coins[0]] + 1, dp[0][j]);
+            }
+        }
+
+        for (int i=1; i<coins.length; i++) {
+            int coin = coins[i];
+            for (int j=1; j<=amount; j++) {
+                if (j < coin || dp[i][j-coin] == Integer.MAX_VALUE) {
+                    dp[i][j] = dp[i-1][j];
+                    continue;
+                }
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-coin]+1);
+            }
+        }
+        return dp[coins.length-1][amount] == Integer.MAX_VALUE ? -1 : dp[coins.length-1][amount];
+    }
+
+    public static int coinChange3(int[] coins, int amount) { // 1d array
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i=1; i<=amount; i++) {
+                if (i < coin || dp[i-coin] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i-coin]+1);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 
 //    public static int rst = Integer.MAX_VALUE;
