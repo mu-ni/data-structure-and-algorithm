5d06ffe1259250929fe1fe689a6e0b54bd1ad073
muni <efrei.muni@gmail.com>
2019-08-16 00:46:56 +0800

    sudoku

---
+++ src/algorithm/RecursionAndDP/SolveSudoku.java
@@ -0,0 +1,52 @@
+package algorithm.RecursionAndDP;
+
+import java.util.*;
+
+public class SolveSudoku {
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'},
+        };
+        solveSudoku(board);
+        System.out.println(Arrays.deepToString(board));
+    }
+
+    public static void solveSudoku(char[][] board) {
+        isSolved(board);
+    }
+
+    public static boolean isSolved(char[][] board) {
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char k='1'; k<='9'; k++) {
+                    if(!isValid(board, i, j, k)) continue;
+                    board[i][j] = k;
+                    if(isSolved(board)) return true;
+                    board[i][j] = '.';
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // TODO
+    private static boolean isValid(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] != '.' && board[i][col] == c) return false; //check row
+            if(board[row][i] != '.' && board[row][i] == c) return false; //check column
+            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' &&
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false; //check 3*3 block
+        }
+        return true;
+    }
+}
