6132d7cbd43ba92b7dd348734a71267cc73330a3
muni <efrei.muni@gmail.com>
2019-08-16 00:48:03 +0800

    opt

--- src/algorithm/RecursionAndDP/SolveSudoku.java
+++ src/algorithm/RecursionAndDP/SolveSudoku.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class SolveSudoku {
     public static void main(String[] args) {
         char[][] board = new char[][]{
-                {'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'},
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'},
         };
         solveSudoku(board);
         System.out.println(Arrays.deepToString(board));
@@ -24,13 +24,13 @@ public class SolveSudoku {
     }
 
     public static boolean isSolved(char[][] board) {
-        for (int i=0; i<9; i++) {
-            for (int j=0; j<9; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') continue;
-                for (char k='1'; k<='9'; k++) {
-                    if(!isValid(board, i, j, k)) continue;
-                    board[i][j] = k;
-                    if(isSolved(board)) return true;
+                for (char c = '1'; c <= '9'; c++) {
+                    if (!isValid(board, i, j, c)) continue;
+                    board[i][j] = c;
+                    if (isSolved(board)) return true;
                     board[i][j] = '.';
                 }
                 return false;
@@ -40,11 +40,11 @@ public class SolveSudoku {
     }
 
     // TODO
-    private static boolean isValid(char[][] board, int row, int col, char c){
-        for(int i = 0; i < 9; i++) {
-            if(board[i][col] != '.' && board[i][col] == c) return false; //check row
-            if(board[row][i] != '.' && board[row][i] == c) return false; //check column
-            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' &&
+    private static boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] != '.' && board[i][col] == c) return false; //check row
+            if (board[row][i] != '.' && board[row][i] == c) return false; //check column
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' &&
                     board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false; //check 3*3 block
         }
         return true;
