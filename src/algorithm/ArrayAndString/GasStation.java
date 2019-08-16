46458fa5ef7564f3c6b0e2d0af87ce1549e0a05c
muni <muni@4paradigm.com>
2019-08-16 12:06:01 +0800

    gas

---
+++ src/algorithm/ArrayAndString/GasStation.java
@@ -0,0 +1,29 @@
+package algorithm.ArrayAndString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GasStation {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,1,6,6};
+        System.out.println(gasStation(7,7,arr));
+    }
+
+    // n - drive n kms with full petrol
+    // k - number of gas station
+    // arr - distance between k ~ k-1
+    // starting with full petrol
+    public static int gasStation(int n, int k, int[] arr) { // not fully tested
+        List<Integer> rst = new ArrayList<>();
+        int cur = n; // current petrol
+        for (int i=0; i<=k; i++) {
+            if (arr[i] > cur) {
+                cur = n;
+                rst.add(i);
+            }
+            cur -= arr[i];
+            if (cur < 0) return -1;
+        }
+        return rst.size();
+    }
+}
