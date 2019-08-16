42c51b46f0e210dd5e9fdeeb97b3f4eaaeb2a61e
muni <muni@4paradigm.com>
2019-08-16 21:49:57 +0800

    ad

--- src/algorithm/TreeAndGraph/PathSum2.java
+++ src/algorithm/TreeAndGraph/PathSum2.java
@@ -12,16 +12,21 @@ public class PathSum2 {
 
     public static List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> rst = new ArrayList<>();
-
+        dfs(rst, root, sum, new ArrayList<>());
         return rst;
     }
 
     public static void dfs(List<List<Integer>> rst, TreeNode node, int sum, List<Integer> path) {
-        if (path.stream().mapToInt(Integer::intValue).sum() == sum) {
+        if (node == null) return;
+        path.add(node.val);
+        sum -= node.val;
+
+        if (node.left == null && node.right == null && sum == 0) {
             rst.add(new ArrayList<>(path));
-            return;
+        } else {
+            dfs(rst, node.left, sum, path);
+            dfs(rst, node.right, sum, path);
         }
-
-
+        path.remove(path.size() - 1);
     }
 }
