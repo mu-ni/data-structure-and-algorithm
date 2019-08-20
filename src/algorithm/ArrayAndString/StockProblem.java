012456c839d443d7d4aa2c1512a6c960cf49019e
muni <muni@4paradigm.com>
2019-08-20 14:36:45 +0800

    stock

---
+++ src/algorithm/ArrayAndString/StockProblem.java
@@ -0,0 +1,18 @@
+package algorithm.ArrayAndString;
+
+public class StockProblem {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int min = prices[0];
+        int max = 0;
+        for (int cur : prices) {
+            max = Math.max(max, cur - min);
+            min = Math.min(min, cur);
+        }
+        return max;
+    }
+}
