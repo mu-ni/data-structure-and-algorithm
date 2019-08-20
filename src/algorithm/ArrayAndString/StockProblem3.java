8a8c6828c2991013d229709f6ad9ff29ada08728
muni <muni@4paradigm.com>
2019-08-20 20:19:35 +0800

    opt stock

--- src/algorithm/ArrayAndString/StockProblem3.java
+++ src/algorithm/ArrayAndString/StockProblem3.java
@@ -2,23 +2,21 @@ package algorithm.ArrayAndString;
 
 public class StockProblem3 {
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{3,3,5,0,0,3,1,4}));
+        System.out.println(maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
     }
 
     // at most two transactions
     public static int maxProfit(int[] prices) {
-        int b1 = Integer.MAX_VALUE; //1 buy
-        int sum1 = 0;//1 buy 1 sell
-        int b2 = Integer.MAX_VALUE;//2 buy
-        int sum2 = 0;//2 buy 2 sell
-        for (int p : prices) {
-            b1 = Math.min(b1, p);
-            sum1 = Math.max(sum1, p - b1);
-            b2 = Math.min(b2, p - sum1);
-            sum2 = Math.max(sum2, p - b2);
-            System.out.println(b1 + "," + sum1 + "," + b2 + "," + sum2);
+        int buy1 = Integer.MAX_VALUE;
+        int sell1 = 0;
+        int buy2 = Integer.MAX_VALUE;
+        int sell2 = 0;
+        for (int price : prices) {
+            buy1 = Math.min(buy1, price);
+            sell1 = Math.max(sell1, price - buy1);
+            buy2 = Math.min(buy2, price - sell1);
+            sell2 = Math.max(sell2, price - buy2);
         }
-
-        return sum2;
+        return sell2;
     }
 }
--- src/algorithm/ArrayAndString/StockProblem4.java
+++ src/algorithm/ArrayAndString/StockProblem4.java
@@ -1,4 +1,33 @@
 package algorithm.ArrayAndString;
 
+import java.util.Arrays;
+
 public class StockProblem4 {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(2, new int[]{1,2,4,2,5,7,2,4,9,0}));
+    }
+
+    public static int maxProfit(int k, int[] prices) {
+        if (k >= prices.length/2) return greedy(prices);
+
+        int[] buy = new int[k+1];
+        Arrays.fill(buy,Integer.MAX_VALUE);
+        int[] sell = new int[k+1];
+
+        for (int price : prices) {
+            for (int i=k; i>=1; i--) {
+                buy[i] = Math.min(buy[i], price - sell[i-1]);
+                sell[i] = Math.max(sell[i], price - buy[i]);
+            }
+        }
+        return sell[k];
+    }
+
+    public static int greedy(int[] prices) {
+        int profit = 0;
+        for (int i=1; i<prices.length; i++) {
+            profit += Math.max(prices[i]-prices[i-1], 0);
+        }
+        return profit;
+    }
 }
