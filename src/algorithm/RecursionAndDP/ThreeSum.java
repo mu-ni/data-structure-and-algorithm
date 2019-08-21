a63197ba89069539546e20e9eb2de2e9c8251e83
muni <muni@4paradigm.com>
2019-08-21 18:10:58 +0800

    opt 3sum

--- src/algorithm/RecursionAndDP/ThreeSum.java
+++ src/algorithm/RecursionAndDP/ThreeSum.java
@@ -7,9 +7,39 @@ import java.util.List;
 public class ThreeSum {
     public static void main(String[] args) {
         System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(threeSum2(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> rst = new ArrayList<>();
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i];
+            int head = i + 1;
+            int tail = nums.length - 1;
+            while (head < tail) {
+                if (nums[head] + nums[tail] == target) {
+                    rst.add(Arrays.asList(nums[i], nums[head], nums[tail]));
+                    head++;
+                    tail--;
+                    while (head < tail && nums[head] == nums[head - 1]) {
+                        head++;
+                    }
+                    while (head < tail && nums[tail] == nums[tail + 1]) {
+                        tail--;
+                    }
+                } else if (nums[head] + nums[tail] > target) {
+                    tail--;
+                } else {
+                    head++;
+                }
+            }
+        }
+        return rst;
+    }
+
+    public static List<List<Integer>> threeSum2(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> rst = new ArrayList<>();
         threeSum(rst, nums, 0, 0, new ArrayList<>());
@@ -23,11 +53,11 @@ public class ThreeSum {
             return;
         }
 
-        for (int i=start; i<nums.length; i++) {
-            if (i != start && nums[i] == nums[i-1]) continue;
+        for (int i = start; i < nums.length; i++) {
+            if (i != start && nums[i] == nums[i - 1]) continue;
             path.add(nums[i]);
-            threeSum(rst, nums, sum+nums[i], i+1, path);
-            path.remove(path.size()-1);
+            threeSum(rst, nums, sum + nums[i], i + 1, path);
+            path.remove(path.size() - 1);
         }
     }
 }
