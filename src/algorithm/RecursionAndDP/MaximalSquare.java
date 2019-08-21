8dd9743abf777ecd6394b484a96fce884bfd82e4
muni <efrei.muni@gmail.com>
2019-08-21 22:56:37 +0800

    add m2

--- src/algorithm/RecursionAndDP/MaximalSquare.java
+++ src/algorithm/RecursionAndDP/MaximalSquare.java
@@ -4,9 +4,9 @@ public class MaximalSquare {
     public static void main(String[] args) {
         MaximalSquare obj = new MaximalSquare();
         char[][] matrix = new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
-//        System.out.println(obj.isSquare(matrix, 0, 0, 1));
-//        System.out.println(obj.isSquare(matrix, 1, 2, 1));
         System.out.println(obj.maximalSquare(matrix));
+        System.out.println(obj.maximalSquare2(matrix));
+        System.out.println(obj.maximalSquare3(matrix));
     }
 
     int max = 0;
@@ -39,5 +39,44 @@ public class MaximalSquare {
             }
         }
         return true;
+    }
+
+    // O(mn)
+    // O(mn)
+    public int maximalSquare2(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int[][] dp = new int[matrix.length+1][matrix[0].length+1];
+        int max = 0;
+        for (int i=1; i<=matrix.length; i++) {
+            for (int j=1; j<=matrix[0].length; j++) {
+                if (matrix[i-1][j-1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max*max;
+    }
+
+    // O(mn)
+    // O(n)
+    public int maximalSquare3(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int[] dp = new int[matrix[0].length+1];
+        int max = 0;
+        int prev = 0;
+        for (int i=1; i<=matrix.length; i++) {
+            for (int j=1; j<=matrix[0].length; j++) {
+                int tmp = dp[j];
+                if (matrix[i-1][j-1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j-1], dp[j]), prev)+1;
+                    max = Math.max(max, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                prev = tmp;
+            }
+        }
+        return max*max;
     }
 }
