e8a84b5b524e1e4eef12cc9b0bb0218dfc0567ff
muni <efrei.muni@gmail.com>
2019-08-22 22:52:04 +0800

    .

--- src/algorithm/TreeAndGraph/CanFinishCourse.java
+++ src/algorithm/TreeAndGraph/CanFinishCourse.java
@@ -1,41 +1,78 @@
 package algorithm.TreeAndGraph;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class CanFinishCourse {
     public static void main(String[] args) {
         System.out.println(new CanFinishCourse().canFinish(3, new int[][]{{2,0},{2,1}}));
+        System.out.println(new CanFinishCourse().canFinish2(3, new int[][]{{2,0},{2,1}}));
     }
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) { // adjMatrix, bfs
         int[][] matrix = new int[numCourses][numCourses];
         int[] indegree = new int[numCourses];
-
         for (int i=0; i<prerequisites.length; i++) {
-            int ready = prerequisites[i][0];
+            int cur = prerequisites[i][0];
             int pre = prerequisites[i][1];
-            indegree[ready]++; //
-            matrix[pre][ready] = 1;
+            indegree[cur]++;
+            matrix[pre][cur] = 1;
         }
 
-        Queue<Integer> queue = new LinkedList();
+        Queue<Integer> queue = new LinkedList<>();
         for (int i=0; i<indegree.length; i++) {
-            if (indegree[i] == 0) queue.offer(i);
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
         }
 
         int count = 0;
         while (!queue.isEmpty()) {
-            int course = queue.poll();
+            int pre = queue.poll();
             count++;
-            for (int i=0; i<numCourses; i++) {
-                if (matrix[course][i] != 0) {
-                    indegree[i]--;
-                    if (indegree[i] == 0) {
-                        queue.offer(i);
+            for (int cur=0; cur<numCourses; cur++) {
+                if (matrix[pre][cur] == 1) {
+                    indegree[cur]--;
+                    if (indegree[cur] == 0) {
+                        queue.offer(cur);
                     }
                 }
             }
+        }
+
+        return count == numCourses;
+    }
+
+    public boolean canFinish2(int numCourses, int[][] prerequisites) { // adjList, bfs
+        List<Integer>[] list = new List[numCourses];
+        for (int i=0; i<list.length; i++) {
+            list[i] = new ArrayList<>();
+        }
+        int[] indegree = new int[numCourses];
+        for (int i=0; i<prerequisites.length; i++) {
+            int cur = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            list[pre].add(cur);
+            indegree[cur]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i=0; i<indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            for (int cur : list[pre]) {
+                indegree[cur]--;
+                if (indegree[cur] == 0) queue.offer(cur);
+            }
+            count++;
         }
 
         return count == numCourses;
