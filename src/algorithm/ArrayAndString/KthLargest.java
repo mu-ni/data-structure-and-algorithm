d3389ea879116fb9d330298fa28ca7202ce65b3d
muni <muni@4paradigm.com>
2019-08-22 14:48:29 +0800

    kth largest

--- src/algorithm/ArrayAndString/KthLargest.java
+++ src/algorithm/ArrayAndString/KthLargest.java
@@ -1,10 +1,12 @@
 package algorithm.ArrayAndString;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthLargest {
     public static void main(String[] args) {
         System.out.println(findKthLargest(new int[]{3,2,1,5,6,4}, 2));
+        System.out.println(findKthLargest2(new int[]{3,2,1,5,6,4}, 2));
     }
 
     // O(nlogn)
@@ -14,5 +16,16 @@ public class KthLargest {
         return nums[nums.length-k];
     }
 
-    //TODO partition
+    // O(n) ~ O(n^2)
+    // O(1)
+    public static int findKthLargest2(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int n : nums) {
+            queue.offer(n);
+            if (queue.size() > k) {
+                queue.poll(); // poll smallest
+            }
+        }
+        return queue.peek();
+    }
 }
--- src/algorithm/ArrayAndString/thirdMax.java
+++ src/algorithm/ArrayAndString/thirdMax.java
@@ -3,6 +3,7 @@ package algorithm.ArrayAndString;
 public class thirdMax {
     public static void main(String[] args) {
         System.out.println(thirdMax(new int[]{2,2,1,3}));
+        System.out.println(thirdMax2(new int[]{-2147483648,-2147483648,-2147483648,-2147483648,1,1,1}));
     }
 
     public static int thirdMax(int[] nums) {
@@ -19,6 +20,30 @@ public class thirdMax {
         for (int n : nums) {
             if (n == l1 || n == l2) continue;
             l3 = l3 == null ? n : Math.max(l3, n);
+        }
+        return l3 == null ? l1 : l3;
+    }
+
+    public static int thirdMax2(int[] nums) {
+        Integer l1 = null;
+        Integer l2 = null;
+        Integer l3 = null;
+        for (Integer n : nums) {
+            if (n.equals(l1) || n.equals(l2) || n.equals(l3)) continue;
+            if (l1 == null || n > l1) {
+                l3 = l2;
+                l2 = l1;
+                l1 = n;
+                continue;
+            }
+            if (l2 == null || n > l2) {
+                l3 = l2;
+                l2 = n;
+                continue;
+            }
+            if (l3 == null || n > l3) {
+                l3 = n;
+            }
         }
         return l3 == null ? l1 : l3;
     }
