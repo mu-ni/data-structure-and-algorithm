5d8a2a1c1bf8f5da69b707c672a59f9054d382d3
muni <efrei.muni@gmail.com>
2019-08-24 17:14:53 +0800

    add dfs

--- src/algorithm/TreeAndGraph/BuildOrder.java
+++ src/algorithm/TreeAndGraph/BuildOrder.java
@@ -1,17 +1,16 @@
 package algorithm.TreeAndGraph;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class BuildOrder {
     public static void main(String[] args) {
         char[] projects = new char[]{'a','b','c','d','e','f'};
         char[][] dependencies = new char[][]{{'a','d'},{'f','b'},{'b','d'},{'f','a'},{'d','c'}};
         System.out.println(Arrays.toString(new BuildOrder().buildOrder(projects, dependencies)));
+        System.out.println(Arrays.toString(new BuildOrder().buildOrder2(projects, dependencies)));
     }
 
-    public char[] buildOrder(char[] projects, char[][] dependencies) {
+    public char[] buildOrder(char[] projects, char[][] dependencies) { //bfs
         int[] indegree = new int[projects.length];
         for (int i=0; i<dependencies.length; i++) {
 //            char pre = dependencies[i][0];
@@ -46,5 +45,48 @@ public class BuildOrder {
         }
 
         return index == projects.length ? order : new char[0];
+    }
+
+    public char[] buildOrder2(char[] projects, char[][] dependencies) {// dfs
+        List<Character>[] lists = new List[projects.length];
+        for (char pre : projects) {
+            lists[pre - 'a'] = new ArrayList<>();
+        }
+        int[] indegree = new int[projects.length];
+        for (int i=0; i<dependencies.length; i++) {
+            char pre = dependencies[i][0];
+            char cur = dependencies[i][1];
+            lists[pre - 'a'].add(cur);
+            indegree[cur - 'a']++;
+        }
+
+        int[] visited = new int[projects.length];
+        Stack<Character> stack = new Stack<>();
+        for (char pre : projects) {
+            if (!topoSort(lists, pre, visited, stack)) return new char[0];
+        }
+
+        char[] order = new char[projects.length];
+        int index = 0;
+        while(!stack.isEmpty()) {
+            char pre = stack.pop();
+            order[index++] = pre;
+        }
+
+        return order;
+    }
+
+    public boolean topoSort(List<Character>[] lists, char pre, int[] visited, Stack<Character> stack) {
+        if (visited[pre - 'a'] == 1) return false;
+        if (visited[pre - 'a'] == 2) return true;
+
+        visited[pre-'a'] = 1;
+        for (char cur : lists[pre - 'a']) {
+            if (!topoSort(lists, cur, visited, stack)) return false;
+        }
+
+        visited[pre - 'a'] = 2;
+        stack.push(pre);
+        return true;
     }
 }
