d00db9beaa9651087ed0c3ef07c5a6ed8345464c
muni <muni@4paradigm.com>
2019-08-27 11:08:10 +0800

    ud

--- src/algorithm/ArrayAndString/PrintDiamond.java
+++ src/algorithm/ArrayAndString/PrintDiamond.java
@@ -8,7 +8,7 @@ public class PrintDiamond {
     public void printDiamond(int n) {
         for (int i=0; i<n; i++) {
             for (int k=0; k<n; k++) {
-                if (i >=n/4 && i <= n/2 && k >= n/2 - i && k <= n/2 + i) {
+                if (i >=n/3 && i <= n/2 && k >= n/2 - i && k <= n/2 + i) {
                     System.out.print("*");
                     continue;
                 }
