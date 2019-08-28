7daf5912f5504b42b31f9218efa0108afa8af01f
muni <muni@4paradigm.com>
2019-08-28 19:57:31 +0800

    unique path

--- src/algorithm/RecursionAndDP/Grid/UniquePath.java
+++ src/algorithm/RecursionAndDP/Grid/UniquePath.java
@@ -3,46 +3,84 @@ package algorithm.RecursionAndDP.Grid;
 public class UniquePath {
     public static void main(String[] args) {
         System.out.println(uniquePaths(36, 7));
+        System.out.println(uniquePaths1(36, 7));
         System.out.println(uniquePaths2(36, 7));
         System.out.println(uniquePaths3(36, 7));
+        System.out.println(uniquePaths4(36, 7));
     }
 
     public static int uniquePaths(int m, int n) {
         int[][] grid = new int[m][n];
         for(int i=0; i< m; i++) {
             for(int j=0; j<n; j++) {
-                if(i == 0 || j == 0) {
+                if(i==0 && j==0) {
                     grid[i][j] = 1;
-                } else {
-                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                    continue;
                 }
+                if(i == 0) {
+                    grid[i][j] = grid[i][j-1];
+                    continue;
+                }
+                if(j == 0) {
+                    grid[i][j] = grid[i-1][j];
+                    continue;
+                }
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
             }
         }
         return grid[m-1][n-1];
     }
 
-    public static int uniquePaths2(int r, int c) {
+    public static int uniquePaths1(int m, int n) { //2d
+        int[][] grid = new int[m][n];
+        for(int i=0; i< m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == 0 || j == 0) {
+                    grid[i][j] = 1;
+                    continue;
+                }
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+            }
+        }
+        return grid[m-1][n-1];
+    }
+
+    public static int uniquePaths2(int m, int n) { //1d
+        int[] grid = new int[n];
+        for(int i=0; i< m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == 0 || j == 0) {
+                    grid[j] = 1;
+                    continue;
+                }
+                grid[j] += grid[j-1];
+            }
+        }
+        return grid[n-1];
+    }
+
+    public static int uniquePaths3(int r, int c) {
         if(r == 1 || c == 1) {
             return 1;
         }
 
-        return uniquePaths2(r-1, c) + uniquePaths2(r, c-1);
+        return uniquePaths3(r-1, c) + uniquePaths3(r, c-1);
     }
 
-    public static int uniquePaths3(int m, int n) {
-        return backtrack(m,n);
+    public static int uniquePaths4(int m, int n) {
+        return dfs(m,n);
     }
 
     static int rst = 0;
-    public static int backtrack(int x, int y) {
+    public static int dfs(int x, int y) {
         if (x == 1 && y == 1) {
             rst++;
         }
         if (x >1) {
-            backtrack(x-1, y);
+            dfs(x-1, y);
         }
         if (y >1) {
-            backtrack(x, y-1);
+            dfs(x, y-1);
         }
         return rst;
     }
--- src/algorithm/RecursionAndDP/Grid/UniquePath2.java
+++ src/algorithm/RecursionAndDP/Grid/UniquePath2.java
@@ -2,30 +2,59 @@ package algorithm.RecursionAndDP.Grid;
 
 public class UniquePath2 {
     public static void main(String[] args) {
-        System.out.print(uniquePaths(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+        System.out.println(uniquePaths(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+        System.out.println(uniquePaths2(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
     }
 
-    public static int uniquePaths(int[][] obstacleGrid) {
+    public static int uniquePaths(int[][] obstacleGrid) { //2d
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
+        int[][] grid = new int[m][n];
         for(int i=0; i< m; i++) {
             for(int j=0; j<n; j++) {
                 if(obstacleGrid[i][j] == 1) {
-                    obstacleGrid[i][j] = 0;
-                } else {
-                    if(i==0 && j==0) {
-                        obstacleGrid[i][j] = 1;
-                    } else if(i==0 && j >0) {
-                        obstacleGrid[i][j] = obstacleGrid[i][j-1];
-                    } else if(i>0 && j==0) {
-                        obstacleGrid[i][j] = obstacleGrid[i-1][j];
-                    } else {
-                        obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                    }
+                    grid[i][j] = 0;
+                    continue;
+                }
+                if(i==0 && j==0) {
+                    grid[i][j] = 1;
+                    continue;
+                }
+                if(i == 0) {
+                    grid[i][j] = grid[i][j-1];
+                    continue;
+                }
+                if(j == 0) {
+                    grid[i][j] = grid[i-1][j];
+                    continue;
+                }
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+
+    public static int uniquePaths2(int[][] obstacleGrid) { //1d
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] grid = new int[n];
+        for(int i=0; i< m; i++) {
+            for(int j=0; j<n; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    grid[j] = 0;
+                    continue;
+                }
+                if(i==0 && j==0) {
+                    grid[j] = 1;
+                    continue;
+                }
+                if (j > 0) {
+                    grid[j] += grid[j-1];
                 }
             }
         }
 
-        return obstacleGrid[m-1][n-1];
+        return grid[n-1];
     }
 }
