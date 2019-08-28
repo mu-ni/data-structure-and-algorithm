cfaba25d1a2e4b5f06fb3ba85de623283b2157fe
muni <muni@4paradigm.com>
2019-08-28 17:31:25 +0800

    count & say

---
+++ src/algorithm/RecursionAndDP/CountAndSay.java
@@ -0,0 +1,30 @@
+package algorithm.RecursionAndDP;
+
+public class CountAndSay {
+
+	public static void main(String[] args) {
+		System.out.println(countAndSay(5));
+	}
+
+	public static String countAndSay(int n){
+		if (n == 1) return "1";
+		String s = countAndSay(n-1);
+
+		StringBuilder sb = new StringBuilder();
+		int count = 1;
+		for (int i=0; i<s.length(); i++) {
+			char say = s.charAt(i);
+			if (i == s.length()-1) {
+				sb.append(count).append(say);
+				continue;
+			}
+			if (say == s.charAt(i+1)) {
+				count++;
+				continue;
+			}
+			sb.append(count).append(say);
+			count = 1;
+		}
+		return sb.toString();
+	}
+}
