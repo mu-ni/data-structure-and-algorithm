cca301049f12b00e4ef7000d24c2506c6ec217b1
muni <muni@4paradigm.com>
2019-08-28 14:58:50 +0800

    divide

--- src/algorithm/RecursionAndDP/Divide.java
+++ src/algorithm/RecursionAndDP/Divide.java
@@ -3,12 +3,7 @@ package algorithm.RecursionAndDP;
 public class Divide {
     public static void main(String[] args) {
         System.out.println(divide(123, 45));
-        System.out.println(divide(20, 20));
-        System.out.println(divide(0, 20));
-        System.out.println(divide(40, 10));
-        System.out.println(divide(7, -3));
-        System.out.println(divide(7, 0));
-        System.out.println(divide(10, 3));
+        System.out.println(divide2(10, 3));
     }
 
     public static int divide(int dividend, int divisor) { // 2147483647/2 -> stack overflow
@@ -26,7 +21,38 @@ public class Divide {
         if (dividend < 0) {
             return rst;
         }
-        return helper(dividend, divisor, rst+1);
+        return helper(dividend, divisor, rst + 1);
     }
 
+    public static int divide2(int dividend, int divisor) {
+        if ((dividend == Integer.MIN_VALUE) && divisor == -1) return Integer.MAX_VALUE;
+//        if (divisor == 1) return dividend;
+//        if (dividend == 0) return 0;
+
+        boolean isNegative = false;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+            isNegative = true;
+        }
+
+        long dend = Math.abs((long) dividend);
+        long sor = Math.abs((long) divisor);
+
+        int result = 0;
+        long sum = sor;
+        int count = 1;
+        while (dend >= sor) {
+            if (dend > sum) {
+                sum += sum;
+                count += count;
+            }
+            if (dend < sum) {
+                sum = sor;
+                count = 1;
+            }
+            dend -= sum;
+            result += count;
+        }
+
+        return isNegative ? -result : result;
+    }
 }
