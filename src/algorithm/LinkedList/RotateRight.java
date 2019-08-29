db90ffbb57becaf3d03babd1d4b0c90297d70c19
muni <muni@4paradigm.com>
2019-08-29 14:26:08 +0800

    add m2

--- src/algorithm/LinkedList/RotateRight.java
+++ src/algorithm/LinkedList/RotateRight.java
@@ -6,6 +6,7 @@ public class RotateRight {
     public static void main(String[] args) {
         System.out.println(rotateRight(ListNode.getList(new int[]{1,2,3,4,5}), 2).toString());
         System.out.println(rotateRight(ListNode.getList(new int[]{0,1,2}), 4).toString());
+        System.out.println(rotateRight2(ListNode.getList(new int[]{1,2,3,4,5}), 2).toString());
     }
 
     public static ListNode rotateRight(ListNode head, int k) {
@@ -31,6 +32,30 @@ public class RotateRight {
         fast.next = head;
         head = slow.next;
         slow.next = null;
+        return head;
+    }
+
+    public static ListNode rotateRight2(ListNode head, int k) {
+        if (head == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        int len = 1;
+        while (fast.next != null) {
+            fast = fast.next;
+            len++;
+        }
+
+        k = k%len;
+        for (int i=0; i<len-k-1; i++) {
+            slow = slow.next;
+        }
+
+        if (slow == null) return head;
+        fast.next = head;
+        head = slow.next;
+        slow.next = null;
+
         return head;
     }
 }
