e311568a1cf7e20750ddebaeebe976962732df12
muni <efrei.muni@gmail.com>
2019-08-30 21:55:40 +0800

    spiral2

---
+++ src/algorithm/ArrayAndString/Matrix/SpiralMatrix2.java
@@ -0,0 +1,50 @@
+package algorithm.ArrayAndString.Matrix;
+
+import java.util.Arrays;
+
+public class SpiralMatrix2 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(generateMatrix(3)));
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+
+        int top = 0;
+        int bottom = n-1;
+        int left = 0;
+        int right = n-1;
+        while(true) {
+            for (int i=left; i<=right; i++) {
+                matrix[top][i] = num;
+                num++;
+            }
+            top++;
+            if (top > bottom || left > right) break;
+
+            for (int i=top; i<=bottom; i++) {
+                matrix[i][right] = num;
+                num++;
+            }
+            right--;
+            if (top > bottom || left > right) break;
+
+            for (int i=right; i>=left; i--) {
+                matrix[bottom][i] = num;
+                num++;
+            }
+            bottom--;
+            if (top > bottom || left > right) break;
+
+            for (int i=bottom; i>=top; i--) {
+                matrix[i][left] = num;
+                num++;
+            }
+            left++;
+            if (top > bottom || left > right) break;
+        }
+
+        return matrix;
+    }
+}
