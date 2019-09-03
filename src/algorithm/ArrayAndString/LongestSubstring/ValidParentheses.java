44272ebeab6e9d2510e39fcfd07322a0f932c6a8
muni <muni@4paradigm.com>
2019-09-03 20:31:19 +0800

    ud

--- src/algorithm/ArrayAndString/LongestSubstring/ValidParentheses.java
+++ src/algorithm/ArrayAndString/LongestSubstring/ValidParentheses.java
@@ -4,20 +4,35 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public static void main(String[] args) {
-        System.out.println(new ValidParentheses().longestValidParentheses("()(())"));
+        System.out.println(new ValidParentheses().longestValidParentheses(")()()"));
+        System.out.println(new ValidParentheses().longestValidParentheses2(")()()"));
     }
 
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
         int max = 0;
-        for(int i=0; i<s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == ')' && !stack.isEmpty() && s.charAt(stack.peek()) == '(') {
                 stack.pop();
-                max = Math.max(max, stack.isEmpty() ? i+1 : i - stack.peek());
+                max = Math.max(max, stack.isEmpty() ? i + 1 : i - stack.peek());
                 continue;
             }
             stack.push(i);
+        }
+        return max;
+    }
+
+    public int longestValidParentheses2(String s) {
+        int[] dp = new int[s.length()];
+        int max = 0;
+        for (int i = 1; i < s.length(); i++) {
+            int prevLeft = i - dp[i - 1] - 1;
+            if (s.charAt(i) == ')' && prevLeft >= 0 && s.charAt(prevLeft) == '(') {
+                int prevLen = prevLeft - 1 >= 0 ? dp[prevLeft - 1] : 0;
+                dp[i] = dp[i - 1] + 2 + prevLen;
+                max = Math.max(max, dp[i]);
+            }
         }
         return max;
     }
--- src/algorithm/SearchAndSort/SearchRange.java
+++ src/algorithm/SearchAndSort/SearchRange.java
@@ -5,9 +5,50 @@ import java.util.Arrays;
 public class SearchRange {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new SearchRange().searchRange(new int[]{5,7,7,8,8,10}, 8)));
+        System.out.println(Arrays.toString(new SearchRange().searchRange2(new int[]{5,7,7,8,8,10}, 8)));
     }
 
     public int[] searchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int lo = -1;
+        int hi = -1;
+        while (left <= right) {
+            int mid = (left+right)/2;
+            if (nums[mid] < target) {
+                left = mid+1;
+                continue;
+            }
+            if (nums[mid] > target) {
+                right = mid-1;
+                continue;
+            }
+            lo = mid;
+            right = mid-1;
+        }
+
+        left = 0;
+        right = nums.length-1;
+        while (left <= right) {
+            int mid = (left+right)/2;
+            if (nums[mid] > target) {
+                right = mid-1;
+                continue;
+            }
+            if (nums[mid] < target) {
+                left = mid+1;
+                continue;
+            }
+            hi = mid;
+            left = mid+1;
+        }
+
+        if (lo > hi) return new int[]{-1, -1};
+
+        return new int[]{lo, hi};
+    }
+
+    public int[] searchRange2(int[] nums, int target) { //O(n)
         int start = 0;
         int end = nums.length-1;
         while (start <= end) {
