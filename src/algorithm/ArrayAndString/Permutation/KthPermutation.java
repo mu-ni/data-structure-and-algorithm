c1036d0584ee8e7acdfdbfb6654aa8cd65584d1b
muni <muni@4paradigm.com>
2019-09-04 16:22:23 +0800

    opt

--- src/algorithm/ArrayAndString/Permutation/KthPermutation.java
+++ src/algorithm/ArrayAndString/Permutation/KthPermutation.java
@@ -5,28 +5,28 @@ import java.util.List;
 
 public class KthPermutation {
     public static void main(String[] args) {
-        System.out.println(new KthPermutation().getPermutation(3, 3));
+        System.out.println(new KthPermutation().getPermutation2(4, 5));
     }
 
-    public String getPermutation(int n, int k) {
-        List<String> list = new ArrayList<>();
-        for (int i=1; i<=n; i++) {
-            list.add(String.valueOf(i));
+    public String getPermutation2(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
         }
 
-        int[] factorial = new int[n+1];
+        int[] factorial = new int[n + 1];
         factorial[0] = 1;
-        for (int i=1; i<=n; i++) {
-            factorial[i] = factorial[i-1]*i;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
         }
 
-        k = k-1;
         StringBuilder sb = new StringBuilder();
-        for (int i=1; i<=n; i++) {
-            int index = k/factorial[n-i];
-            sb.append(list.get(index));
-            list.remove(index);
-            k = k%factorial[n-i];
+        k--;
+        while (n > 0) {
+            int i = k / factorial[n - 1];
+            k = k % factorial[n - 1];
+            sb.append(list.remove(i));
+            n--;
         }
 
         return sb.toString();
