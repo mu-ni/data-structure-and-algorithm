ca16f42d53dee3f9e36b758c3b8a628cf8e9f278
muni <efrei.muni@gmail.com>
2019-09-07 20:01:14 +0800

    add

--- src/algorithm/RecursionAndDP/Grid/NQueens2.java
+++ src/algorithm/RecursionAndDP/Grid/NQueens2.java
@@ -7,16 +7,45 @@ public class NQueens2 {
         System.out.println(nQueens2.totalNQueens2(8));
     }
 
-    public int rst = 0;
+    int rst = 0;
     public int totalNQueens(int n) {
+        boolean[] cols = new boolean[n];
+        boolean[] dia1 = new boolean[n*2];
+        boolean[] dia2 = new boolean[n*2];
+        backtrack(n, 0, cols, dia1, dia2);
+        return rst;
+    }
+
+    public void backtrack(int n, int row, boolean[] cols, boolean[] dia1, boolean[] dia2) {
+        if (row == n) {
+            rst++;
+            return;
+        }
+        for (int col=0; col < n; col++) {
+            int d1 = row+col;
+            int d2 = row-col+n;
+            if (cols[col] || dia1[d1] || dia2[d2]) continue;
+
+            cols[col] = true;
+            dia1[d1] = true;
+            dia2[d2] = true;
+            backtrack(n, row+1, cols, dia1, dia2);
+            cols[col] = false;
+            dia1[d1] = false;
+            dia2[d2] = false;
+        }
+    }
+
+    public int rst2 = 0;
+    public int totalNQueens2(int n) {
         boolean[][] board = new boolean[n][n];
         dfs(board, 0);
-        return rst;
+        return rst2;
     }
 
     public void dfs(boolean[][] board, int colIndex) {
         if (colIndex == board.length) {
-            rst++;
+            rst2++;
             return;
         }
 
@@ -45,34 +74,4 @@ public class NQueens2 {
         return x1 == x2 || y1 == y2 || x1 + y1 == x2 + y2 || x1 + y2 == x2 + y1;
     }
 
-
-    public int rst2 = 0;
-    public int totalNQueens2(int n) {
-        boolean[] rows = new boolean[n];
-        boolean[] dia1 = new boolean[n*2]; // x1+y1 = x2+y2
-        boolean[] dia2 = new boolean[n*2]; // x1+y2 = x2+y1 // x1-y1+n = x2-y2+n
-        dfs(0, rows, dia1, dia2);
-        return rst2;
-    }
-
-    public void dfs(int colIndex, boolean[] rows, boolean[] dia1, boolean[] dia2) {
-        if (colIndex == rows.length) {
-            rst2++;
-            return;
-        }
-        for (int i=0; i<rows.length; i++) {
-            int d1 = i + colIndex;
-            int d2 = i - colIndex + rows.length;
-            if (rows[i] || dia1[d1] || dia2[d2]) {
-                continue;
-            }
-            rows[i] = true;
-            dia1[d1] = true;
-            dia2[d2] = true;
-            dfs(colIndex+1, rows, dia1, dia2);
-            rows[i] = false;
-            dia1[d1] = false;
-            dia2[d2] = false;
-        }
-    }
 }
