0f22219f8bf78cfbe50acc0ad4a6cebaeb209d7f
muni <efrei.muni@gmail.com>
2019-09-07 20:56:06 +0800

    add

--- src/algorithm/TreeAndGraph/UniqueBST.java
+++ src/algorithm/TreeAndGraph/UniqueBST.java
@@ -12,12 +12,12 @@ public class UniqueBST {
     public static int numTrees(int n) {
         int[] dp = new int[n+1];
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i=2; i<=n; i++) {
+        for (int i=1; i<=n; i++) {
             for (int j=1; j<=i; j++) {
-                dp[i] += dp[j - 1] * dp[i - j];
+                dp[i] += dp[j-1]*dp[i-j];
             }
         }
+
         return dp[n];
     }
 
