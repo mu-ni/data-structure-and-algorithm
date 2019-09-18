21e0781f225e5c57fad8625ba64e40a4cc593a6d
muni <muni@4paradigm.com>
2019-09-18 18:04:54 +0800

    update

--- src/algorithm/RecursionAndDP/Grid/FloodFill.java
+++ src/algorithm/RecursionAndDP/Grid/FloodFill.java
@@ -7,26 +7,26 @@ public class FloodFill {
     public static void main(String[] args) {
         int[][] filled = floodFill(new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 2);
         System.out.println(Arrays.deepToString(filled));
+        filled = floodFill(new int[][]{{0,0,0}, {0,1,1}}, 1, 1, 1);
+        System.out.println(Arrays.deepToString(filled));
     }
 
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int oldColor = image[sr][sc];
-        if (oldColor == newColor) return image;
+        if (image.length == 0) return image;
+        if (image[sr][sc] == newColor) return image;
 
-        fill(image, sr, sc, oldColor, newColor);
+        fill(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
 
-    public static void fill(int[][] image, int r, int c, int oldColor, int newColor) {
-        if (r < 0 || c < 0 || r > image.length - 1 || c > image[0].length - 1) {
-            return;
-        }
-        if (image[r][c] == oldColor) {
-            image[r][c] = newColor;
-            fill(image, r - 1, c, oldColor, newColor);
-            fill(image, r + 1, c, oldColor, newColor);
-            fill(image, r, c - 1, oldColor, newColor);
-            fill(image, r, c + 1, oldColor, newColor);
-        }
+    public static void fill(int[][] image, int x, int y, int oldColor, int newColor) {
+        if (x < 0 || y < 0 || x >= image.length || y >= image[0].length) return;
+        if (image[x][y] != oldColor) return;
+
+        image[x][y] = newColor;
+        fill(image, x-1, y, oldColor, newColor);
+        fill(image, x+1, y, oldColor, newColor);
+        fill(image, x, y-1, oldColor, newColor);
+        fill(image, x, y+1, oldColor, newColor);
     }
 }
