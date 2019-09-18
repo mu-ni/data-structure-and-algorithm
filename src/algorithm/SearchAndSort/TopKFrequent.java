efae8ccf8ff85f06f692c424cc904a3cbcc804e7
muni <muni@4paradigm.com>
2019-09-18 17:39:08 +0800

    renew

--- src/algorithm/SearchAndSort/TopKFrequent.java
+++ src/algorithm/SearchAndSort/TopKFrequent.java
@@ -4,32 +4,32 @@ import java.util.*;
 
 public class TopKFrequent {
     public static void main(String[] args) {
-        System.out.println(new TopKFrequent().topKFrequent(new int[]{5,1,1,1,2,2,3,4}, 2));
+        System.out.println(new TopKFrequent().topKFrequent(new int[]{1}, 1));
     }
 
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int n : nums) {
-            int count = map.getOrDefault(n, 0);
-            map.put(n, count+1);
+            map.put(n, map.getOrDefault(n, 0)+1);
         }
 
-        List[] bucket = new List[nums.length+1];
-        for(int n:map.keySet()){
-            int freq = map.get(n);
-            if(bucket[freq]==null) {
-                bucket[freq] = new LinkedList<>();
+        List[] freqList = new List[nums.length+1];
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (freqList[freq] == null) {
+                freqList[freq] = new ArrayList();
             }
-            bucket[freq].add(n);
+            freqList[freq].add(key);
         }
 
-        List<Integer> list = new ArrayList<>();
-        for (int i=bucket.length-1; i>=0 && k >0; i--) {
-            if (bucket[i] == null) continue;
-            list.addAll(bucket[i]);
-            k -= bucket[i].size();
+        List<Integer> rst = new ArrayList<>();
+        for (int i=freqList.length-1; i>=0; i--) {
+            if (freqList[i] == null) continue;
+            rst.addAll(freqList[i]);
+            k -= freqList[i].size();
+            if (k <= 0) break;
         }
 
-        return list;
+        return rst;
     }
 }
