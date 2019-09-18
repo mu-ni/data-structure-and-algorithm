c46b2a0b157c6587390c9d32948bcb5768aa969d
muni <efrei.muni@gmail.com>
2019-09-18 23:14:33 +0800

    rename

--- src/algorithm/TreeAndGraph/PathSum3.java
+++ src/algorithm/TreeAndGraph/PathSum3.java
@@ -14,12 +14,12 @@ public class PathSum3 {
         return dfs(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
 
-    public static int dfs(TreeNode node, int sum) {
+    public static int dfs(TreeNode node, int rem) {
         if (node == null) return 0;
-        sum -= node.val;
-        if (sum == 0) {
-            return 1 + dfs(node.left, sum) + dfs(node.right, sum);
+        rem -= node.val;
+        if (rem == 0) {
+            return 1 + dfs(node.left, rem) + dfs(node.right, rem);
         }
-        return dfs(node.left, sum) + dfs(node.right, sum);
+        return dfs(node.left, rem) + dfs(node.right, rem);
     }
 }
