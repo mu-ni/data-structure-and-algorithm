2a16f3f6c4e00adf2ae736d84033f8b7fbd2713e
muni <efrei.muni@gmail.com>
2019-09-22 10:25:00 +0800

    reorder log

---
+++ src/algorithm/ArrayAndString/ContainsDup3.java
@@ -0,0 +1,39 @@
+package algorithm.ArrayAndString;
+
+import java.util.TreeSet;
+
+public class ContainsDup3 {
+    public static void main(String[] args) {
+        System.out.println(new ContainsDup3().containsNearbyAlmostDuplicate(new int[]{1,2,3,1}, 3,0));
+        System.out.println(new ContainsDup3().containsNearbyAlmostDuplicate2(new int[]{1,2,3,1}, 3,0));
+    }
+
+    // time limit exceeded
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<=i+k && j < nums.length; j++) {
+                if (Math.abs((long)nums[i] - (long)nums[j]) <= (long)t) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i=0; i<nums.length; i++) {
+            // successor
+            Integer s = set.ceiling(nums[i]);
+            if (s != null && s <= nums[i] + t) return true;
+
+            // predecessor
+            Integer p = set.floor(nums[i]);
+            if (p != null && nums[i] <= p + t) return true;
+
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
