dbb4c33ceeae7accbd16a2103759ce2a69830766
muni <muni@4paradigm.com>
2019-09-23 14:05:29 +0800

    add

---
+++ src/algorithm/RecursionAndDP/RemoveInvalidParentheses.java
@@ -0,0 +1,52 @@
+package algorithm.RecursionAndDP;
+
+import java.util.*;
+
+public class RemoveInvalidParentheses {
+    public static void main(String[] args) {
+        System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses("(a)())()"));
+    }
+
+    // bfs
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> rst = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        queue.offer(s);
+        visited.add(s);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean found = false;
+            for (int i=0; i<size; i++) {
+                String str = queue.poll();
+                if (isValid(str)) {
+                    rst.add(str);
+                    found = true;
+                    continue;
+                }
+
+                for (int j=0; j<str.length(); j++) {
+                    if (str.charAt(j) != '(' && str.charAt(j) != ')') continue;
+                    String sub = str.substring(0, j) + str.substring(j+1);
+                    if (visited.contains(sub)) continue;
+                    queue.add(sub);
+                    visited.add(sub);
+                }
+            }
+            if (found) break;
+        }
+
+        return rst;
+    }
+
+    public boolean isValid(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            if (c == ')') count--;
+            if (count < 0) return false;
+        }
+        return count == 0;
+    }
+}
