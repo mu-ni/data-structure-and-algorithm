0260931d427472fcb73023a52c2d0c72ffd24acf
muni <muni@4paradigm.com>
2019-09-23 14:19:59 +0800

    add

--- src/algorithm/RecursionAndDP/RemoveInvalidParentheses.java
+++ src/algorithm/RecursionAndDP/RemoveInvalidParentheses.java
@@ -5,9 +5,11 @@ import java.util.*;
 public class RemoveInvalidParentheses {
     public static void main(String[] args) {
         System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses("(a)())()"));
+        System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses2("(a)())()"));
+        System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses3("(a)())()"));
     }
 
-    // bfs
+    // bfs - min remove
     public List<String> removeInvalidParentheses(String s) {
         List<String> rst = new ArrayList<>();
         Queue<String> queue = new LinkedList<>();
@@ -38,6 +40,58 @@ public class RemoveInvalidParentheses {
         }
 
         return rst;
+    }
+
+    // bfs - all solutions
+    public List<String> removeInvalidParentheses3(String s) {
+        List<String> rst = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        queue.offer(s);
+        visited.add(s);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                String str = queue.poll();
+                if (isValid(str)) {
+                    rst.add(str);
+                    continue;
+                }
+
+                for (int j=0; j<str.length(); j++) {
+                    if (str.charAt(j) != '(' && str.charAt(j) != ')') continue;
+                    String sub = str.substring(0, j) + str.substring(j+1);
+                    if (visited.contains(sub)) continue;
+                    queue.add(sub);
+                    visited.add(sub);
+                }
+            }
+        }
+
+        return rst;
+    }
+
+    // dfs - all solutions
+    public List<String> removeInvalidParentheses2(String s) {
+        List<String> rst = new ArrayList<>();
+        dfs(rst, s, new HashSet<>());
+        return rst;
+    }
+
+    public void dfs(List<String> rst, String s, HashSet<String> visited) {
+        if (isValid(s)) {
+            rst.add(s);
+            return;
+        }
+
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) != '(' && s.charAt(i) != ')') continue;
+            String sub = s.substring(0, i) + s.substring(i+1);
+            if (visited.contains(sub)) continue;
+            visited.add(sub);
+            dfs(rst, sub, visited);
+        }
     }
 
     public boolean isValid(String s) {
