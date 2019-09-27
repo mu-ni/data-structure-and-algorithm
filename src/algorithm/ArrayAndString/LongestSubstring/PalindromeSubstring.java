1ae2afa74fd3225e228f03106fad9411c7f70a85
muni <muni@4paradigm.com>
2019-09-27 19:12:02 +0800

    add

--- src/algorithm/ArrayAndString/LongestSubstring/PalindromeSubstring.java
+++ src/algorithm/ArrayAndString/LongestSubstring/PalindromeSubstring.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class PalindromeSubstring {
     public static void main(String[] args) {
         System.out.println(longestPalindrome("babad"));
-        System.out.println(longestPalindrome2("babad"));
+        System.out.println(longestPalindrome2("a"));
     }
 
     public static String longestPalindrome(String s) {
@@ -34,25 +34,21 @@ public class PalindromeSubstring {
     }
 
     public static String longestPalindrome2(String s) {
-        if (s.length() <= 1) return s;
         String rst = "";
-        for (int i=1; i<s.length(); i++) {
+        for (int i=0; i<s.length(); i++) {
             String s1 = expand(s, i, i);
-            String s2 = expand(s, i-1, i);
-            int max = Math.max(s1.length(), s2.length());
-            if (max > rst.length()) {
-                rst = s1.length() == max ? s1 : s2;
-            }
+            String s2 = expand(s, i, i+1);
+            String longer = s1.length() >= s2.length() ? s1:s2;
+            rst = longer.length() >= rst.length() ? longer:rst;
         }
         return rst;
     }
 
-    public static String expand(String s, int slow, int fast) {
-        while (slow >= 0 && fast < s.length()) {
-            if (s.charAt(slow) != s.charAt(fast)) break;
-            slow--;
-            fast++;
+    public static String expand(String s, int c1, int c2) {
+        while (c1 >=0 && c2 < s.length() && s.charAt(c1) == s.charAt(c2)) {
+            c1--;
+            c2++;
         }
-        return s.substring(slow+1, fast);
+        return s.substring(c1+1, c2);
     }
 }
