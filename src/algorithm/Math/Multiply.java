9cfca73e796f0659a3dca5bd72f71a20542029bf
muni <efrei.muni@gmail.com>
2019-09-28 20:46:53 +0800

    add

--- src/algorithm/Math/Multiply.java
+++ src/algorithm/Math/Multiply.java
@@ -8,14 +8,14 @@ public class Multiply {
 
     public static String multiply(String num1, String num2) {
         int[] dp = new int[num1.length() + num2.length()];
-        for (int i=num1.length()-1; i>=0; i--) {
-            for(int j=num2.length()-1;j>=0;j--) {
-                int n1 = num1.charAt(i)-'0';
-                int n2 = num2.charAt(j)-'0';
-                int sum = n1*n2 + dp[i+j+1];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                int sum = n1 * n2 + dp[i + j + 1];
 
-                dp[i+j+1] = sum%10;
-                dp[i+j] += sum/10;
+                dp[i + j + 1] = sum % 10;
+                dp[i + j] += sum / 10;
             }
         }
 
@@ -24,7 +24,7 @@ public class Multiply {
             if (sb.length() == 0 && i == 0) continue;
             sb.append(i);
         }
-        return sb.length() == 0 ? "0":sb.toString();
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 
     public static int multiply(int num1, int num2) {
@@ -37,9 +37,9 @@ public class Multiply {
         if (smaller == 0) return 0;
         if (smaller == 1) return bigger;
 
-        int half = smaller/2;
-        if (smaller%2 == 1) {
-            return helper(half, bigger) + helper(half+1, bigger);
+        int half = smaller / 2;
+        if (smaller % 2 == 1) {
+            return helper(half, bigger) + helper(half + 1, bigger);
         }
         return helper(half, bigger) + helper(half, bigger);
     }
