463ea48c1ea4307c5d6756927e6e40cadf2a08be
muni <muni@4paradigm.com>
2019-09-30 13:57:43 +0800

    add

--- src/algorithm/Tree/BTFromInorderPostorder.java
+++ src/algorithm/Tree/BTFromInorderPostorder.java
@@ -11,9 +11,35 @@ public class BTFromInorderPostorder {
         int[] postorder = new int[]{9,15,7,20,3};
         TreeNode tree = new BTFromInorderPostorder().buildTree(inorder, postorder);
         System.out.println(TreeNode.tree2arr(tree));
+        tree = new BTFromInorderPostorder().buildTree2(inorder, postorder);
+        System.out.println(TreeNode.tree2arr(tree));
     }
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    public TreeNode helper(int[] inorder, int i_start, int i_end, int[] postorder, int p_start, int p_end) {
+        if (p_start > p_end) return null;
+        if (p_start == p_end) return new TreeNode(postorder[p_start]);
+
+        int root_val = postorder[p_end];
+        int i_root_index = 0;
+        for (int i = i_start; i <= i_end; i++) {
+            if (inorder[i] == root_val) {
+                i_root_index = i;
+                break;
+            }
+        }
+
+        int left_len = i_root_index - i_start;
+        TreeNode root = new TreeNode(root_val);
+        root.left = helper(inorder, i_start, i_root_index -1, postorder, p_start, p_start + left_len - 1);
+        root.right = helper(inorder, i_root_index + 1, i_end, postorder, p_start + left_len, p_end - 1);
+        return root;
+    }
+
+    public TreeNode buildTree2(int[] inorder, int[] postorder) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i=0; i<inorder.length; i++) {
             map.put(inorder[i], i);
@@ -28,10 +54,10 @@ public class BTFromInorderPostorder {
         int root_val = postorder[p_end];
         TreeNode root = new TreeNode(root_val);
         int i_root_index = map.get(root_val);
-        int leftNum = i_root_index - i_start;
+        int left_len = i_root_index - i_start;
 
-        TreeNode left = helper(inorder, i_start, i_root_index-1, postorder, p_start, p_start + leftNum-1, map);
-        TreeNode right = helper(inorder, i_root_index+1, i_end, postorder, p_start + leftNum, p_end-1, map);
+        TreeNode left = helper(inorder, i_start, i_root_index-1, postorder, p_start, p_start + left_len-1, map);
+        TreeNode right = helper(inorder, i_root_index+1, i_end, postorder, p_start + left_len, p_end-1, map);
         root.left = left;
         root.right = right;
 
--- src/algorithm/Tree/BTFromPreorderInorder.java
+++ src/algorithm/Tree/BTFromPreorderInorder.java
@@ -2,13 +2,12 @@ package algorithm.Tree;
 
 import algorithm.Tree.Dao.TreeNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BTFromPreorderInorder {
     public static void main(String[] args) {
-        int[] preorder = new int[]{3,9,20,15,7};
-        int[] inorder = new int[]{9,3,15,20,7};
+        int[] preorder = new int[]{3, 9, 20, 15, 7};
+        int[] inorder = new int[]{9, 3, 15, 20, 7};
         TreeNode tree = new BTFromPreorderInorder().buildTree(preorder, inorder);
         System.out.println(TreeNode.tree2arr(tree));
         tree = new BTFromPreorderInorder().buildTree2(preorder, inorder);
@@ -16,7 +15,7 @@ public class BTFromPreorderInorder {
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+        return helper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
     public TreeNode helper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end) {
@@ -24,29 +23,27 @@ public class BTFromPreorderInorder {
         if (p_start == p_end) return new TreeNode(preorder[p_start]);
 
         int root_val = preorder[p_start];
-        TreeNode root = new TreeNode(root_val);
         int i_root_index = 0;
-        for (int i=i_start; i<=i_end; i++) {
+        for (int i = i_start; i <= i_end; i++) {
             if (inorder[i] == root_val) {
                 i_root_index = i;
                 break;
             }
         }
 
-        int leftNum = i_root_index - i_start;
-        TreeNode left = helper(preorder, p_start+1, p_start+leftNum, inorder, i_start, i_root_index-1);
-        TreeNode right = helper(preorder, p_start+leftNum+1, p_end, inorder, i_root_index+1, i_end);
-        root.left = left;
-        root.right = right;
+        int left_len = i_root_index - i_start;
+        TreeNode root = new TreeNode(root_val);
+        root.left = helper(preorder, p_start + 1, p_start + left_len, inorder, i_start, i_root_index - 1);
+        root.right = helper(preorder, p_start + left_len + 1, p_end, inorder, i_root_index + 1, i_end);
         return root;
     }
 
     public TreeNode buildTree2(int[] preorder, int[] inorder) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0; i<inorder.length; i++) {
+        for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
-        return helper2(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1, map);
+        return helper2(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
     }
 
     public TreeNode helper2(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end, Map<Integer, Integer> map) {
@@ -58,8 +55,8 @@ public class BTFromPreorderInorder {
         int i_root_index = map.get(root_val);
 
         int leftNum = i_root_index - i_start;
-        TreeNode left = helper2(preorder, p_start+1, p_start+leftNum, inorder, i_start, i_root_index-1, map);
-        TreeNode right = helper2(preorder, p_start+leftNum+1, p_end, inorder, i_root_index+1, i_end, map);
+        TreeNode left = helper2(preorder, p_start + 1, p_start + leftNum, inorder, i_start, i_root_index - 1, map);
+        TreeNode right = helper2(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end, map);
         root.left = left;
         root.right = right;
         return root;
