acd93e77fee6e1d175d17c4894363dfa89bb8797
muni <efrei.muni@gmail.com>
2019-10-03 16:46:52 +0800

    add

--- src/algorithm/Backtrack/CoinChange.java
+++ src/algorithm/Backtrack/CoinChange.java
@@ -10,6 +10,8 @@ public class CoinChange { // min coins make up amount
         System.out.println(coinChange4(new int[]{1, 2, 3}, 20));
         System.out.println(coinChange5(new int[]{1, 2, 3}, 20));
         System.out.println(coinChange6(new int[]{1, 2, 3}, 20));
+        System.out.println(coinChange7(new int[]{1, 2, 3}, 20));
+        System.out.println(coinChange8(new int[]{1, 2, 3}, 20));
     }
 
     public static int coinChange(int[] coins, int amount) {
@@ -194,4 +196,55 @@ public class CoinChange { // min coins make up amount
 //            path.remove(path.size()-1);
 //        }
 //    }
+
+    public static int coinChange7(int[] coins, int amount) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        Arrays.fill(dp[0], amount+1);
+        for (int i=0; i<coins.length; i++) {
+            dp[i][0] = 0;
+        }
+        int coin0 = coins[0];
+        for (int j=1; j<=amount; j++) {
+            if (j < coin0) {
+                dp[1][j] = amount+1;
+            } else {
+                dp[1][j] = Math.min(dp[1][j-coin0]+1, dp[0][j]);
+            }
+        }
+
+        for (int i=2; i<= coins.length; i++) {
+            int coin = coins[i-1];
+            for (int j=1; j<=amount; j++) {
+                if (j >= coin) {
+                    dp[i][j] = Math.min(dp[i][j-coin] + 1, dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[coins.length][amount] == amount+1 ? -1 : dp[coins.length][amount];
+    }
+
+    public static int coinChange8(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        int coin0 = coins[0];
+        for (int j=1; j<=amount; j++) {
+            if (j < coin0) continue;
+            dp[j] = Math.min(dp[j-coin0]+1, dp[j]);
+        }
+
+        for (int i=1; i< coins.length; i++) {
+            int coin = coins[i];
+            for (int j=1; j<=amount; j++) {
+                if (j >= coin) {
+                    dp[j] = Math.min(dp[j-coin] + 1, dp[j]);
+                }
+            }
+        }
+
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
 }
