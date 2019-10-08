14ac45ecf09f43139f8d810fb5534d6fd874bb65
muni <muni@4paradigm.com>
2019-10-08 19:55:24 +0800

    add

--- src/algorithm/Backtrack/Permutations2.java
+++ src/algorithm/Backtrack/Permutations2.java
@@ -13,27 +13,27 @@ public class Permutations2 {
     public static List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> rst = new ArrayList<>();
         Arrays.sort(nums);
-        boolean[] used = new boolean[nums.length];
-        dfs(rst, nums, new ArrayList<>(), used);
+        boolean[] visited = new boolean[nums.length];
+        dfs(rst, nums, new ArrayList<>(), visited);
         return rst;
     }
 
-    public static void dfs(List<List<Integer>> rst, int[] nums, List<Integer> path, boolean[] used) {
+    public static void dfs(List<List<Integer>> rst, int[] nums, List<Integer> path, boolean[] visited) {
         if(nums.length == path.size()) {
             rst.add(new ArrayList<>(path));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if(used[i] || i != 0 && nums[i] == nums[i-1] && !used[i-1]) {
+            if(visited[i] || i != 0 && nums[i] == nums[i-1] && !visited[i-1]) {
                 continue;
             }
 
             path.add(nums[i]);
-            used[i] = true;
-            dfs(rst, nums, path, used);
+            visited[i] = true;
+            dfs(rst, nums, path, visited);
             path.remove(path.size()-1);
-            used[i] = false;
+            visited[i] = false;
         }
     }
 }
