01081861eefd79abbbbfd575135610e99e7163de
muni <muni@4paradigm.com>
2019-10-09 14:47:05 +0800

    add

--- src/algorithm/ArrayAndString/LongestSubstring/AtMostKTypesCharacter.java
+++ src/algorithm/ArrayAndString/LongestSubstring/AtMostKTypesCharacter.java
@@ -1,64 +1,61 @@
 package algorithm.ArrayAndString.LongestSubstring;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AtMostKTypesCharacter {
     public static void main(String[] args) {
-        System.out.println(longestSubstring("ababc", 3));
-        System.out.println(longestSubstring("abcab", 3));
-        System.out.println(longestSubstring2("ababc", 3));
-        System.out.println(longestSubstring2("abcab", 3));
+        System.out.println(lengthOfLongestSubstringKDistinct("eceba", 2));
+        System.out.println(lengthOfLongestSubstringKDistinct2("eceba", 2));
     }
 
-    // character types < k
-    public static int longestSubstring(String s, int k) { // not fully tested
+    // character types <= k
+    // TLE
+    public static int lengthOfLongestSubstringKDistinct(String s, int k) {
         int slow = 0;
         int fast = 0;
         int max = 0;
-        while(slow < s.length() && fast < s.length()) {
-            String sub = s.substring(slow, fast+1);
-            if (isValid(sub, k)) {
+        while(fast < s.length()) {
+            int types = getTypes(s, slow, fast);
+            if (types <= k) {
                 fast++;
-                max = Math.max(max, fast - slow);
-                continue;
+                max = Math.max(max, fast-slow);
+            } else {
+                slow++;
             }
-            slow++;
         }
         return max;
     }
 
-    public static boolean isValid(String s, int k) {
-        int[] arr = new int[26];
-        for (char c : s.toCharArray()) {
-            arr[c - 'a']++;
+    public static int getTypes(String s, int slow, int fast) {
+        Set<Character> set = new HashSet<>();
+        for (int i=slow; i<=fast; i++) {
+            set.add(s.charAt(i));
         }
-        int types = 0;
-        for (int i : arr) {
-            if (i != 0) types++;
-            if (types >= k) return false;
-        }
-        return true;
+        return set.size();
     }
 
-    public static int longestSubstring2(String s, int k) {
+    public static int lengthOfLongestSubstringKDistinct2(String s, int k) {
+        int[] arr = new int[128];
         int slow = 0;
         int fast = 0;
         int max = 0;
-        int[] arr = new int[26];
         int types = 0;
-        while(slow < s.length() && fast < s.length()) {
-            if (arr[s.charAt(fast) - 'a'] == 0) {
+        while (fast < s.length()) {
+            if (arr[s.charAt(fast)] == 0) {
                 types++;
             }
-            arr[s.charAt(fast) - 'a']++;
-            if (types <k) {
-                fast++;
-                max = Math.max(max, fast-slow);
-                continue;
+            arr[s.charAt(fast)]++;
+            fast++;
+            if (types <= k) {
+                max = Math.max(max, fast - slow);
+            } else {
+                arr[s.charAt(slow)]--;
+                if (arr[s.charAt(slow)] == 0) {
+                    types--;
+                }
+                slow++;
             }
-            arr[s.charAt(slow) - 'a']--;
-            if (arr[s.charAt(slow) - 'a'] == 0) {
-                types--;
-            }
-            slow++;
         }
         return max;
     }
--- src/algorithm/TwoPointers/LongestRepeatingCharacterReplacement.java
+++ src/algorithm/TwoPointers/LongestRepeatingCharacterReplacement.java
@@ -2,7 +2,8 @@ package algorithm.TwoPointers;
 
 public class LongestRepeatingCharacterReplacement {
     public static void main(String[] args) {
-        System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement("ABAB", 2));
+        System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement("AABABBA", 1));
+        System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement2("AABABBA", 1));
     }
 
     public int characterReplacement(String s, int k) {
@@ -20,6 +21,23 @@ public class LongestRepeatingCharacterReplacement {
                 slow++;
             }
             max = Math.max(max, fast - slow);
+        }
+        return max;
+    }
+
+    public int characterReplacement2(String s, int k) {
+        int[] arr = new int[128];
+        int slow = 0;
+        int max = 0;
+        int count = 0;
+        for (int fast = 0; fast < s.length(); fast++) {
+            arr[s.charAt(fast)]++;
+            count = Math.max(count, arr[s.charAt(fast)]);
+            if (fast - slow + 1 > count + k) {
+                arr[s.charAt(slow)]--;
+                slow++;
+            }
+            max = Math.max(max, fast-slow+1);
         }
         return max;
     }
