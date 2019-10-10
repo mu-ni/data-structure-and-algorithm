f2e7034b52812c34e130dbe9c5175f8118649eee
muni <muni@4paradigm.com>
2019-10-10 14:25:56 +0800

    add

---
+++ src/algorithm/SearchAndSort/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,43 @@
+package algorithm.SearchAndSort;
+
+import java.util.Arrays;
+
+public class ShortestUnsortedContinuousSubarray {
+    public static void main(String[] args) {
+        System.out.println(new ShortestUnsortedContinuousSubarray().findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 9, 15}));
+        System.out.println(new ShortestUnsortedContinuousSubarray().findUnsortedSubarray2(new int[]{2, 6, 4, 8, 10, 9, 15}));
+    }
+
+    public int findUnsortedSubarray(int[] nums) {
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        int start = -1;
+        int end = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (start == -1 && nums[i] != sorted[i]) {
+                start = i;
+            }
+            if (start != -1 && nums[i] != sorted[i]) {
+                end = i;
+            }
+        }
+
+        return start == -1 ? 0 : end - start + 1;
+    }
+
+    public int findUnsortedSubarray2(int[] nums) {
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        int start = 0;
+        while (start < nums.length && nums[start] == sorted[start]) {
+            start++;
+        }
+        int end = nums.length - 1;
+        while (end >= 0 && nums[end] == sorted[end]) {
+            end--;
+        }
+        return start > end ? 0 : end - start + 1;
+    }
+}
