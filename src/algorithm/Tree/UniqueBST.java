8ab8aca3878c18b457693d915dea4be7ea926def
muni <muni@4paradigm.com>
2019-10-10 18:07:55 +0800

    add

--- src/algorithm/Tree/UniqueBST.java
+++ src/algorithm/Tree/UniqueBST.java
@@ -4,6 +4,7 @@ public class UniqueBST {
     public static void main(String[] args) {
         System.out.println(numTrees(18));
         System.out.println(numTrees2(18));
+        System.out.println(numTrees3(18));
     }
 
     // f(3) =
@@ -17,11 +18,23 @@ public class UniqueBST {
                 dp[i] += dp[j-1]*dp[i-j];
             }
         }
-
         return dp[n];
     }
 
     public static int numTrees2(int n) {
+        if (n <= 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                dp[i] += dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+
+    public static int numTrees3(int n) {
         if (n <= 1) return 1;
         int sum = 0;
         for (int i=1; i<=n; i++) {
