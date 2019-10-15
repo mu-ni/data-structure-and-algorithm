273dac99022a5035b5590309d715f62972d9fca2
muni <muni@4paradigm.com>
2019-10-15 13:26:04 +0800

    add

--- src/algorithm/ArrayAndString/LongestWordInDictionary.java
+++ src/algorithm/ArrayAndString/LongestWordInDictionary.java
@@ -8,9 +8,25 @@ public class LongestWordInDictionary {
     public static void main(String[] args) {
         String[] words = new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"};
         System.out.println(new LongestWordInDictionary().longestWord(words));
+        System.out.println(new LongestWordInDictionary().longestWord2(words));
     }
 
     public String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        String rst = "";
+        for (String word : words) {
+            boolean exist = true;
+            for (int i=0; i<word.length(); i++) {
+                String sub = word.substring(0, i+1);
+                if (!set.contains(sub)) exist = false;
+            }
+            if (exist && word.length() > rst.length()) rst = word;
+        }
+        return rst;
+    }
+
+    public String longestWord2(String[] words) {
         Arrays.sort(words);
         Set<String> set = new HashSet<>();
         String rst = "";
