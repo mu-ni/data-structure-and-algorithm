058609e626db276785dc93ea1a1f300ee74e1ec5
muni <muni@4paradigm.com>
2019-10-15 14:33:01 +0800

    add

--- src/algorithm/UnionFind/ShortestBridge.java
+++ src/algorithm/UnionFind/ShortestBridge.java
@@ -1,14 +1,16 @@
 package algorithm.UnionFind;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class ShortestBridge {
     public static void main(String[] args) {
         int[][] A = new int[][]{
-                {0,1,0},
-                {0,0,0},
-                {0,0,1}
+                {0, 1, 0},
+                {0, 0, 0},
+                {0, 0, 1}
         };
         System.out.println(new ShortestBridge().shortestBridge(A));
     }
@@ -16,57 +18,53 @@ public class ShortestBridge {
     int m;
     int n;
     int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    Queue<int[]> queue = new LinkedList<>();
-
-    // two islands
     public int shortestBridge(int[][] A) {
         m = A.length;
-        if (m == 0) return 0;
+        if (m == 0) return -1;
         n = A[0].length;
 
+        boolean[][] visited = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (A[i][j] == 0) continue;
-                boolean[][] visited = new boolean[m][n];
-                dfs(A, i, j, visited);
-                return minDistance(A, visited);
+                if (A[i][j] == 1) {
+                    Queue<int[]> queue = new LinkedList<>();
+                    dfs(A, i, j, visited, queue);
+                    return minDistance(A, visited, queue);
+                }
             }
         }
-
         return -1;
     }
 
-    // mark one island
-    public void dfs(int[][] A, int x, int y, boolean[][] visited) {
+    public void dfs(int[][] A, int x, int y, boolean[][] visited, Queue<int[]> queue) {
         if (x < 0 || y < 0 || x >= m || y >= n) return;
-        if (visited[x][y] || A[x][y] == 0) return;
+        if (A[x][y] == 0 || visited[x][y]) return;
 
         visited[x][y] = true;
         queue.offer(new int[]{x, y});
-        for (int[] dir : dirs) {
-            dfs(A, x + dir[0], y + dir[1], visited);
-        }
+        dfs(A, x + 1, y, visited, queue);
+        dfs(A, x - 1, y, visited, queue);
+        dfs(A, x, y + 1, visited, queue);
+        dfs(A, x, y - 1, visited, queue);
     }
 
-    // expand dist
-    public int minDistance(int[][] A, boolean[][] visited) {
+    public int minDistance(int[][] A, boolean[][] visited, Queue<int[]> queue) {
         int dist = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                int[] pos = queue.poll();
+                int[] crd = queue.poll();
                 for (int[] dir : dirs) {
-                    int x = pos[0] + dir[0];
-                    int y = pos[1] + dir[1];
+                    int x = crd[0] + dir[0];
+                    int y = crd[1] + dir[1];
                     if (x < 0 || y < 0 || x >= m || y >= n || visited[x][y]) continue;
                     if (A[x][y] == 1) return dist;
-                    queue.offer(new int[]{x, y});
                     visited[x][y] = true;
+                    queue.offer(new int[]{x, y});
                 }
             }
             dist++;
         }
-
         return -1;
     }
 }
