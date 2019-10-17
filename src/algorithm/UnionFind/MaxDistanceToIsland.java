b8f8262076e84b0317af0882ca5895113bb84913
muni <muni@4paradigm.com>
2019-10-17 13:45:44 +0800

    add

--- src/algorithm/UnionFind/MaxDistanceToIsland.java
+++ src/algorithm/UnionFind/MaxDistanceToIsland.java
@@ -11,6 +11,7 @@ public class MaxDistanceToIsland {
                 {1, 0, 1}
         };
         System.out.println(new MaxDistanceToIsland().maxDistance(grid));
+        System.out.println(new MaxDistanceToIsland().maxDistance2(grid));
     }
 
     int m;
@@ -41,17 +42,53 @@ public class MaxDistanceToIsland {
             for (int i = 0; i < size; i++) {
                 int[] pos = queue.poll();
                 for (int[] dir : dirs) {
-                    int crdX = pos[0] + dir[0];
-                    int crdY = pos[1] + dir[1];
-                    if (crdX < 0 || crdY < 0 || crdX >= m || crdY >= n) continue;
-                    if (visited[crdX][crdY]) continue;
-                    if (grid[crdX][crdY] == 1) return dist;
-                    visited[crdX][crdY] = true;
-                    queue.offer(new int[]{crdX, crdY});
+                    int x = pos[0] + dir[0];
+                    int y = pos[1] + dir[1];
+                    if (x < 0 || y < 0 || x >= m || y >= n) continue;
+                    if (visited[x][y]) continue;
+                    if (grid[x][y] == 1) return dist;
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y});
                 }
             }
             dist++;
         }
         return -1;
+    }
+
+    // bfs from 1, count levels of 0 until all visited
+    public int maxDistance2(int[][] grid) {
+        m = grid.length;
+        if (m == 0) return -1;
+        n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    visited[i][j] = true;
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        int max = -1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                int[] pos = queue.poll();
+                for (int[] dir : dirs) {
+                    int x = pos[0] + dir[0];
+                    int y = pos[1] + dir[1];
+                    if (x < 0 || y < 0 || x >= m || y >= n) continue;
+                    if (visited[x][y] || grid[x][y] == 1) continue;
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y});
+                }
+            }
+            max++;
+        }
+        // max = 0 means no valid result
+        return max <= 0 ? -1 : max;
     }
 }
