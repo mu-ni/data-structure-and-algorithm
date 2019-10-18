868b1af665bc0d7aa0c98eccc724fc328737102f
muni <muni@4paradigm.com>
2019-10-18 12:15:26 +0800

    add

--- src/algorithm/Graph/LongestIncreasingPathInMatrix.java
+++ src/algorithm/Graph/LongestIncreasingPathInMatrix.java
@@ -41,26 +41,26 @@ public class LongestIncreasingPathInMatrix {
         if (m == 0) return 0;
         n = matrix[0].length;
         int max = 0;
-        int[][] cache = new int[m][n];
+        int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int len = dfs(matrix, i, j, Integer.MIN_VALUE, cache);
+                int len = dfs(matrix, i, j, Integer.MIN_VALUE, dp);
                 max = Math.max(max, len);
             }
         }
         return max;
     }
 
-    public int dfs(int[][] matrix, int x, int y, int val, int[][] cache) {
+    public int dfs(int[][] matrix, int x, int y, int val, int[][] dp) {
         if (x < 0 || y < 0 || x >= m || y >= n) return 0;
         if (matrix[x][y] <= val) return 0;
-        if (cache[x][y] != 0) return cache[x][y];
+        if (dp[x][y] != 0) return dp[x][y];
 
         int max = 0;
         for (int[] dir : dirs) {
-            max = Math.max(max, 1 + dfs(matrix, x + dir[0], y + dir[1], matrix[x][y], cache));
+            max = Math.max(max, 1 + dfs(matrix, x + dir[0], y + dir[1], matrix[x][y], dp));
         }
-        cache[x][y] = max;
+        dp[x][y] = max;
         return max;
     }
 }
