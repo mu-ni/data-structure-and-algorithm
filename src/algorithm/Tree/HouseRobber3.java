78c0e926d9bab5dbedd8b143caea100dd3d0bf49
muni <muni@4paradigm.com>
2019-10-18 20:10:20 +0800

    add

---
+++ src/algorithm/Tree/HouseRobber3.java
@@ -0,0 +1,24 @@
+package algorithm.Tree;
+
+import algorithm.Tree.Dao.TreeNode;
+
+public class HouseRobber3 {
+    public static void main(String[] args) {
+        System.out.println(new HouseRobber3().rob(TreeNode.arr2tree(new Integer[]{3, 2, 3, null, 3, null, 1})));
+    }
+
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(robInclude(root), robExclude(root));
+    }
+
+    public int robInclude(TreeNode node) {
+        if (node == null) return 0;
+        return node.val + robExclude(node.left) + robExclude(node.right);
+    }
+
+    public int robExclude(TreeNode node) {
+        if (node == null) return 0;
+        return rob(node.left) + rob(node.right);
+    }
+}
