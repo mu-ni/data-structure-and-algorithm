ae1a509b86f6601925e166eec7115d2eb8d0dbb7
muni <muni@4paradigm.com>
2019-10-18 12:14:53 +0800

    add

--- src/algorithm/Graph/LongestIncreasingPathInMatrix.java
+++ src/algorithm/Graph/LongestIncreasingPathInMatrix.java
@@ -2,37 +2,65 @@ package algorithm.Graph;
 
 public class LongestIncreasingPathInMatrix {
     public static void main(String[] args) {
-        int[][] matrix = new int[][]{{9,9,4},{6,6,8},{2,1,1}};
+        int[][] matrix = new int[][]{{7, 8, 9}, {9, 7, 6}, {7, 2, 3}};
         System.out.println(new LongestIncreasingPathInMatrix().longestIncreasingPath(matrix));
+        System.out.println(new LongestIncreasingPathInMatrix().longestIncreasingPath2(matrix));
     }
 
     int m;
     int n;
-    int[][] dirs = new int[][]{{1,0}, {-1,0},{0,1},{0,-1}};
+    int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    // TLE
     public int longestIncreasingPath(int[][] matrix) {
         m = matrix.length;
         if (m == 0) return 0;
         n = matrix[0].length;
         int max = 0;
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                boolean[][] visited = new boolean[m][n];
-                int len = dfs(matrix, visited, i, j, Integer.MIN_VALUE, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int len = dfs(matrix, i, j, Integer.MIN_VALUE, 0);
                 max = Math.max(max, len);
             }
         }
         return max;
     }
 
-    public int dfs(int[][] matrix, boolean[][] visited, int x, int y, int val, int len) {
+    public int dfs(int[][] matrix, int x, int y, int val, int len) {
         if (x < 0 || y < 0 || x >= m || y >= n) return len;
-        if (visited[x][y] || matrix[x][y] <= val) return len;
-
-        visited[x][y] = true;
+        if (matrix[x][y] <= val) return len;
         int max = len;
         for (int[] dir : dirs) {
-            max = Math.max(max, dfs(matrix, visited, x+dir[0], y+dir[1], matrix[x][y], len+1));
+            max = Math.max(max, dfs(matrix, x + dir[0], y + dir[1], matrix[x][y], len + 1));
         }
+        return max;
+    }
+
+    public int longestIncreasingPath2(int[][] matrix) {
+        m = matrix.length;
+        if (m == 0) return 0;
+        n = matrix[0].length;
+        int max = 0;
+        int[][] cache = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int len = dfs(matrix, i, j, Integer.MIN_VALUE, cache);
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int[][] matrix, int x, int y, int val, int[][] cache) {
+        if (x < 0 || y < 0 || x >= m || y >= n) return 0;
+        if (matrix[x][y] <= val) return 0;
+        if (cache[x][y] != 0) return cache[x][y];
+
+        int max = 0;
+        for (int[] dir : dirs) {
+            max = Math.max(max, 1 + dfs(matrix, x + dir[0], y + dir[1], matrix[x][y], cache));
+        }
+        cache[x][y] = max;
         return max;
     }
 }
