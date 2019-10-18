5215bd60e915c069a8b40b0cd3900674548a91f3
muni <muni@4paradigm.com>
2019-10-18 20:11:59 +0800

    add

--- src/algorithm/Tree/HouseRobber3.java
+++ src/algorithm/Tree/HouseRobber3.java
@@ -4,7 +4,7 @@ import algorithm.Tree.Dao.TreeNode;
 
 public class HouseRobber3 {
     public static void main(String[] args) {
-        System.out.println(new HouseRobber3().rob(TreeNode.arr2tree(new Integer[]{3, 2, 3, null, 3, null, 1})));
+        System.out.println(new HouseRobber3().rob(TreeNode.arr2tree(new Integer[]{4,1,null,2,null,3})));
     }
 
     public int rob(TreeNode root) {
