367f4ff0e57ef38ac5acfbe353123b39fea14964
muni <efrei.muni@gmail.com>
2019-10-22 22:50:35 +0800

    add

--- src/algorithm/SearchAndSort/DynamicSuggestions.java
+++ src/algorithm/SearchAndSort/DynamicSuggestions.java
@@ -8,28 +8,28 @@ import java.util.List;
 
 public class DynamicSuggestions {
     public static void main(String[] args) {
-        String[] repo = new String[]{"mobile", "mouse", "moneypot", "monitor", "mousepad"};
+        List<String> repo = Arrays.asList("mobile", "mouse", "moneypot", "monitor", "mousepad");
         System.out.println(new DynamicSuggestions().dynamicSuggestions(5, repo, "mouse"));
     }
 
-    public List<List<String>> dynamicSuggestions(int num, String[] repo, String query) {
-        Arrays.sort(repo);
-        boolean[] isInvalid = new boolean[repo.length];
+    public List<List<String>> dynamicSuggestions(int num, List<String> repo, String query) {
+        Collections.sort(repo);
         List<List<String>> rst = new ArrayList<>();
         for (int i=1; i<query.length(); i++) {
             String sub = query.substring(0, i+1).toLowerCase();
             List<String> list = new ArrayList<>();
-            for (int j=0; j<repo.length; j++) {
-                String word = repo[j];
-                if (isInvalid[j] || list.contains(word)) continue;
+            for (String word : repo) {
+                if (list.contains(word)) continue;
                 if (word.toLowerCase().startsWith(sub)) {
                     list.add(word);
-                    if (list.size() == 3) break;
-                } else {
-                    isInvalid[j] = true;
                 }
             }
-            rst.add(list);
+            repo = list;
+            if (list.size() > 3) {
+                rst.add(list.subList(0, 3));
+            } else {
+                rst.add(list);
+            }
         }
         return rst;
     }
