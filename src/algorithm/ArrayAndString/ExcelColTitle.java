2c968d26f0a68c87b501c62918a508362d6fcad8
muni <muni@4paradigm.com>
2019-10-24 17:05:43 +0800

    add

--- src/algorithm/ArrayAndString/ExcelColTitle.java
+++ src/algorithm/ArrayAndString/ExcelColTitle.java
@@ -4,6 +4,7 @@ public class ExcelColTitle {
     public static void main(String[] args) {
         System.out.println(new ExcelColTitle().convertToTitle(52));
         System.out.println(new ExcelColTitle().convertToTitle2(52));
+        System.out.println(new ExcelColTitle().convertToTitle3(52));
     }
 
     String dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -23,6 +24,20 @@ public class ExcelColTitle {
             n--;
             sb.insert(0, (char)('A' + n%26));
             n /= 26;
+        }
+        return sb.toString();
+    }
+
+    public String convertToTitle3(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            if (n%26 == 0) {
+                sb.insert(0, dict.charAt(25));
+                n = n/26 - 1;
+            } else {
+                sb.insert(0, dict.charAt(n%26 - 1));
+                n /= 26;
+            }
         }
         return sb.toString();
     }
---
+++ src/algorithm/RecursionAndDP/CountVowelsPermutation.java
@@ -0,0 +1,11 @@
+//package algorithm.RecursionAndDP;
+//
+//public class CountVowelsPermutation {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public int countVowelPermutation(int n) {
+//
+//    }
+//}
