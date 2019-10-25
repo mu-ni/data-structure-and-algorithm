b3f901437f3cafebf8b544c4cc0d7a68f0b14dbe
muni <muni@4paradigm.com>
2019-10-25 19:36:44 +0800

    add

--- src/algorithm/ArrayAndString/StringCompression.java
+++ src/algorithm/ArrayAndString/StringCompression.java
@@ -3,6 +3,7 @@ package algorithm.ArrayAndString;
 public class StringCompression {
     public static void main(String[] args) {
         System.out.println(compress(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
+        System.out.println(compress2(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
     }
 
     public static int compress(char[] chars) {
@@ -23,6 +24,25 @@ public class StringCompression {
             }
             len = 1;
             i++;
+        }
+        return index;
+    }
+
+    public static int compress2(char[] chars) {
+        int len = 1;
+        int index = 0;
+        for (int i=0; i<chars.length; i++) {
+            if (i != chars.length-1 && chars[i] == chars[i+1]) {
+                len++;
+                continue;
+            }
+            chars[index++] = chars[i];
+            if (len != 1) {
+                for (char n : String.valueOf(len).toCharArray()) {
+                    chars[index++] = n;
+                }
+            }
+            len = 1;
         }
         return index;
     }
