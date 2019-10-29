d8f9e14a96e72380982169ad10fc366d78c2c3d0
muni <muni@4paradigm.com>
2019-10-29 21:03:23 +0800

    add

---
+++ src/algorithm/ArrayAndString/SimplifyPath.java
@@ -0,0 +1,28 @@
+package algorithm.ArrayAndString;
+
+import java.util.Stack;
+
+public class SimplifyPath {
+    public static void main(String[] args) {
+        System.out.println(new SimplifyPath().simplifyPath("/../"));
+    }
+
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        for (String s : path.split("/")) {
+            if (!stack.isEmpty() && s.equals("..")) {
+                stack.pop();
+            }
+            if (!s.isEmpty() && !s.equals("..") && !s.equals(".")) {
+                stack.push(s);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop()).insert(0, "/");
+        }
+        String rst = sb.toString();
+        return rst.isEmpty() ? "/" : rst;
+    }
+}
