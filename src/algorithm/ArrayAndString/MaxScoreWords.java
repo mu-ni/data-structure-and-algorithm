4c3066c4438588e110f90a772b21bfc2ebf75ba9
muni <muni@4paradigm.com>
2019-11-22 21:31:38 +0800

    add

--- src/algorithm/ArrayAndString/MaxScoreWords.java
+++ src/algorithm/ArrayAndString/MaxScoreWords.java
@@ -5,7 +5,8 @@ public class MaxScoreWords {
         String[] words = new String[]{"dog","cat","dad","good"};
         char[] letters = new char[]{'a','a','c','d','d','d','g','o','o'};
         int[] score = new int[]{1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
-        System.out.println(new MaxScoreWords().maxScoreWords(words, letters, score));
+//        System.out.println(new MaxScoreWords().maxScoreWords(words, letters, score));
+        System.out.println(new MaxScoreWords().maxScoreWords2(words, letters, score));
     }
 
     // WRONG
@@ -35,5 +36,28 @@ public class MaxScoreWords {
             return true;
         }
         return false;
+    }
+
+    int max = 0;
+    public int maxScoreWords2(String[] words, char[] letters, int[] score) {
+        backtrack(words, letters, score, 0, 0);
+        return max;
+    }
+
+    public void backtrack(String[] words, char[] letters, int[] score, int start, int sum) {
+        max = Math.max(max, sum);
+        for (int i=start; i<words.length; i++) {
+            int count = getScore(words[i], letters,score);
+            backtrack(words, letters, score, i+1, sum + count);
+        }
+    }
+
+    public int getScore(String word, char[] letters, int[] score) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (isValid(letters, c)) return -1;
+            count += score[c - 'a'];
+        }
+        return count;
     }
 }
