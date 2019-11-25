22c6feb1649eaae00b9732a25b01acff301f0858
muni <muni@4paradigm.com>
2019-11-25 14:26:58 +0800

    add

--- src/algorithm/ArrayAndString/MaxScoreWords.java
+++ src/algorithm/ArrayAndString/MaxScoreWords.java
@@ -5,59 +5,37 @@ public class MaxScoreWords {
         String[] words = new String[]{"dog","cat","dad","good"};
         char[] letters = new char[]{'a','a','c','d','d','d','g','o','o'};
         int[] score = new int[]{1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
-//        System.out.println(new MaxScoreWords().maxScoreWords(words, letters, score));
-        System.out.println(new MaxScoreWords().maxScoreWords2(words, letters, score));
+        System.out.println(new MaxScoreWords().maxScoreWords(words, letters, score));
     }
 
-    // WRONG
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        int[] dict = new int[score.length];
+        for (char c : letters) {
+            dict[c - 'a']++;
+        }
+        return backtrack(words, dict, score, 0);
+    }
+
+    public int backtrack(String[] words, int[] dict, int[] score, int start) {
         int max = 0;
-        for (String word : words) {
-            int count = 0;
-            char[] dict = letters.clone();
-            for (char c : word.toCharArray()) {
-                if (!isValid(dict, c)) {
-                    count = 0;
-                    break;
-                }
-                count += score[c - 'a'];
-            }
-            System.out.println(count);
-            max = Math.max(max, count);
-        }
-        return max;
-    }
-
-
-    public boolean isValid (char[] dict, char c) {
-        for (int i=0; i<dict.length; i++) {
-            if (dict[i] != c) continue;
-            dict[i] = '*';
-            return true;
-        }
-        return false;
-    }
-
-    int max = 0;
-    public int maxScoreWords2(String[] words, char[] letters, int[] score) {
-        backtrack(words, letters, score, 0, 0);
-        return max;
-    }
-
-    public void backtrack(String[] words, char[] letters, int[] score, int start, int sum) {
-        max = Math.max(max, sum);
         for (int i=start; i<words.length; i++) {
-            int count = getScore(words[i], letters,score);
-            backtrack(words, letters, score, i+1, sum + count);
+            String word = words[i];
+            int count = 0;
+            boolean isValid = true;
+            for (char c : word.toCharArray()) {
+                dict[c - 'a']--;
+                count += score[c - 'a'];
+                if (dict[c - 'a'] < 0) isValid = false;
+            }
+            if (isValid) {
+                count += backtrack(words, dict, score, i+1);
+                max = Math.max(max, count);
+            }
+            for (char c : word.toCharArray()) {
+                dict[c - 'a']++;
+                count -= score[c - 'a'];
+            }
         }
-    }
-
-    public int getScore(String word, char[] letters, int[] score) {
-        int count = 0;
-        for (char c : word.toCharArray()) {
-            if (isValid(letters, c)) return -1;
-            count += score[c - 'a'];
-        }
-        return count;
+        return max;
     }
 }
