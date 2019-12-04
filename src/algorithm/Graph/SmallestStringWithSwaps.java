05b34c0714df5c47fd83456f6f4a98257998d9d1
muni <muni@4paradigm.com>
2019-12-04 21:27:48 +0800

    add

---
+++ src/algorithm/Graph/SmallestStringWithSwaps.java
@@ -0,0 +1,92 @@
+package algorithm.Graph;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SmallestStringWithSwaps {
+    public static void main(String[] args) {
+        Integer[][] arr = new Integer[][]{{0,3},{1,2}};
+        List<List<Integer>> pairs = Arrays.stream(arr).map(Arrays::asList).collect(Collectors.toList());
+        System.out.println(new SmallestStringWithSwaps().smallestStringWithSwaps("dcab", pairs));
+        System.out.println(new SmallestStringWithSwaps().smallestStringWithSwaps2("dcab", pairs));
+    }
+
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (List<Integer> pair : pairs) {
+            List<Integer> list = graph.getOrDefault(pair.get(0), new ArrayList<>());
+            list.add(pair.get(1));
+            graph.put(pair.get(0), list);
+
+            list = graph.getOrDefault(pair.get(1), new ArrayList<>());
+            list.add(pair.get(0));
+            graph.put(pair.get(1), list);
+        }
+
+        boolean[] visited = new boolean[s.length()];
+        char[] arr = s.toCharArray();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i=0; i<s.length(); i++) {
+            if (visited[i]) continue;
+            queue.offer(i);
+            List<Character> lc = new ArrayList<>();
+            List<Integer> li = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                int cur = queue.poll();
+                if (visited[cur]) continue;
+                visited[cur] = true;
+                lc.add(s.charAt(cur));
+                li.add(cur);
+                List<Integer> nextList = graph.get(cur);
+                if (nextList != null) {
+                    for (int next : nextList) {
+                        queue.offer(next);
+                    }
+                }
+            }
+            Collections.sort(lc);
+            Collections.sort(li);
+            for (int j=0; j<lc.size(); j++) {
+                arr[li.get(j)] = lc.get(j);
+            }
+        }
+
+        return new String(arr);
+    }
+
+    public String smallestStringWithSwaps2(String s, List<List<Integer>> pairs) {
+        int[] p = new int[s.length()];
+        for (int i = 0; i < p.length; i++) p[i] = i;
+        for (List<Integer> pair : pairs) {
+            union(p, pair.get(0), pair.get(1));
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < p.length; i++) {
+            int parent = find(p, i);
+            map.putIfAbsent(parent, new ArrayList<>());
+            map.get(parent).add(i);
+        }
+        char[] arr = s.toCharArray();
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> group = entry.getValue();
+            List<Character> temp = new ArrayList<>();
+            for (int ind : group) temp.add(arr[ind]);
+            Collections.sort(temp);
+            for (int i = 0; i < group.size(); i++) {
+                arr[group.get(i)] = temp.get(i);
+            }
+        }
+        return new String(arr);
+    }
+
+    public void union (int[] p, int nodeA, int nodeB) {
+        int A = find(p, nodeA);
+        int B = find(p, nodeB);
+        if (A != B) p[A] = B;
+    }
+
+    public int find(int[] p, int node) {
+        if (p[node] == node) return node;
+        return p[node] = find(p, p[node]);
+    }
+}
---
+++ src/algorithm/Math/UglyNumber3.java
@@ -0,0 +1,54 @@
+package algorithm.Math;
+
+import java.util.*;
+
+public class UglyNumber3 {
+    public static void main(String[] args) {
+        System.out.println(new UglyNumber3().nthUglyNumber(4, 2, 3, 4));
+        System.out.println(new UglyNumber3().nthUglyNumber2(4, 2, 3, 4));
+    }
+
+    // TLE
+    public int nthUglyNumber(int n, int a, int b, int c) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            set.add(a * i);
+            set.add(b * i);
+            set.add(c * i);
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        return list.get(n - 1);
+    }
+
+    public int nthUglyNumber2(int n, int a, int b, int c) {
+        int left = 1;
+        int right = Integer.MAX_VALUE;
+        int rst = 0;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            int count = count(a, b, c, mid);
+            if (count < n) {
+                left = mid+1;
+            } else {
+                rst = mid;
+                right = mid-1;
+            }
+        }
+        return rst;
+    }
+
+    // how many numbers from 1 to num are divisible by either a, b or c
+    private int count(long a, long b, long c, long num) {
+        return (int) (num/a + num/b + num/c - num/lcm(a,b) - num/lcm(b,c) - num/lcm(a,c) + num/lcm(a, lcm(b,c)));
+    }
+
+    private long gcd(long a, long b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
+    }
+
+    private long lcm(long a, long b) {
+        return (a*b)/gcd(a, b);
+    }
+}
