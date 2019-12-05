db0f24a6fc77fc18bb8207d397c3ebd0d250627c
muni <muni@4paradigm.com>
2019-12-05 17:32:50 +0800

    add

--- src/algorithm/UnionFind/LongestConsecutiveSequence.java
+++ src/algorithm/UnionFind/LongestConsecutiveSequence.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
-//        System.out.println(new LongestConsecutiveSequence().longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
+        System.out.println(new LongestConsecutiveSequence().longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
         System.out.println(new LongestConsecutiveSequence().longestConsecutive(new int[]{0,3,7,2,5,8,4,6,0,1}));
     }
 
@@ -27,4 +27,6 @@ public class LongestConsecutiveSequence {
         }
         return max;
     }
+
+    // TODO union find
 }
