24e53168136215dc0744f353a772cf3b0e97839e
muni <muni@4paradigm.com>
2019-12-05 14:11:45 +0800

    add union find

---
+++ src/algorithm/UnionFind/Dao/BasicUnionFind.java
@@ -0,0 +1,53 @@
+package algorithm.UnionFind.Dao;
+
+
+/**
+ * Created by muni on 2019/12/5
+ */
+public class BasicUnionFind {
+    int count;
+    int[] id;
+
+    public BasicUnionFind(int N) {
+        count = N;
+        id = new int[N];
+        for (int i=0; i<N; i++) {
+            // connect to itself
+            id[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        int pId = find(p);
+        int qId = find(q);
+        for (int i=0; i<id.length; i++) {
+            if (find(i) == pId) {
+                id[i] = qId;
+            }
+        }
+        count--;
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public static void main(String[] args) {
+        BasicUnionFind uf = new BasicUnionFind(8);
+        uf.union(2,3);
+        uf.union(1,0);
+        uf.union(0,4);
+        uf.union(5,7);
+        System.out.println(uf.getCount());
+        System.out.println(uf.isConnected(1,2));
+        System.out.println(uf.isConnected(1,4));
+    }
+}
---
+++ src/algorithm/UnionFind/Dao/UnionFind.java
@@ -0,0 +1,52 @@
+package algorithm.UnionFind.Dao;
+
+/**
+ * Created by muni on 2019/12/5
+ */
+public class UnionFind {
+    int count;
+    int[] id;
+
+    public UnionFind(int N) {
+        count = N;
+        id = new int[N];
+        for (int i=0; i<N; i++) {
+            // connect to itself
+            id[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        int pId = find(p);
+        int qId = find(q);
+        if (pId == qId) return;
+        id[qId] = pId;
+        count--;
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(8);
+        uf.union(2,3);
+        uf.union(1,0);
+        uf.union(0,4);
+        uf.union(5,7);
+        System.out.println(uf.getCount());
+        System.out.println(uf.isConnected(1,2));
+        System.out.println(uf.isConnected(1,4));
+    }
+}
