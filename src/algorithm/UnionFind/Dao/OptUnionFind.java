96af358502e84638c73a766ba1c2560388c0dcee
muni <muni@4paradigm.com>
2019-12-05 18:18:19 +0800

    add

--- src/algorithm/UnionFind/Dao/OptUnionFind.java
+++ src/algorithm/UnionFind/Dao/OptUnionFind.java
@@ -1,16 +1,17 @@
 package algorithm.UnionFind.Dao;
 
-
 /**
  * Created by muni on 2019/12/5
  */
-public class BasicUnionFind {
+public class OptUnionFind {
     int count;
     int[] parent;
+    int[] size;
 
-    public BasicUnionFind(int N) {
+    public OptUnionFind(int N) {
         count = N;
         parent = new int[N];
+        size = new int[N];
         for (int i=0; i<N; i++) {
             // connect to itself
             parent[i] = i;
@@ -19,19 +20,27 @@ public class BasicUnionFind {
 
     // O(n)
     public void union(int p, int q) {
-        int pId = find(p);
-        int qId = find(q);
-        for (int i = 0; i< parent.length; i++) {
-            if (find(i) == pId) {
-                parent[i] = qId;
-            }
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
         }
         count--;
     }
 
-    // O(1)
+    // O(n)
     public int find(int p) {
-        return parent[p];
+        while (p != parent[p]) {
+            // reduce path length
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
     }
 
     public int getCount() {
@@ -43,7 +52,7 @@ public class BasicUnionFind {
     }
 
     public static void main(String[] args) {
-        BasicUnionFind uf = new BasicUnionFind(8);
+        OptUnionFind uf = new OptUnionFind(8);
         uf.union(2,3);
         uf.union(1,0);
         uf.union(0,4);
