fd003af52078438cdecda63d2f40d4038379f366
muni <muni@4paradigm.com>
2019-12-11 14:22:47 +0800

    add

--- src/design/LFUCache.java
+++ src/design/LFUCache.java
@@ -1,17 +1,59 @@
 package design;
 
+import java.util.*;
+
 // Least Frequently Used (LFU)
 public class LFUCache {
-    public LFUCache(int capacity) {
+    int capacity;
+    Map<Integer, Integer> keyValueMap;
+    Map<Integer, Integer> keyCountMap;
+    TreeMap<Integer, List<Integer>> countValueMap;
 
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        keyValueMap = new HashMap<>();
+        keyCountMap = new HashMap<>();
+        countValueMap = new TreeMap<>();
     }
 
     public int get(int key) {
-        return -1;
+        if (!keyValueMap.containsKey(key)) return -1;
+        int count = keyCountMap.get(key)+1;
+        keyCountMap.put(key, count);
+        List<Integer> list = countValueMap.get(count-1);
+        list.remove(Integer.valueOf(key));
+        if (list.size() == 0) {
+            countValueMap.remove(count-1);
+        }
+        countValueMap.putIfAbsent(count, new ArrayList<>());
+        countValueMap.get(count).add(key);
+        return keyValueMap.get(key);
     }
 
     public void put(int key, int value) {
+        if (capacity <= 0) return;
+        if (keyValueMap.containsKey(key)) {
+            keyValueMap.put(key, value);
+            get(key);
+            return;
+        }
 
+        if (keyValueMap.size() >= capacity) {
+            Integer minCount = countValueMap.firstKey();
+            List<Integer> list = countValueMap.get(minCount);
+            Integer remVal = list.get(0);
+            keyValueMap.remove(remVal);
+            keyCountMap.remove(remVal);
+            list.remove(remVal);
+            if (list.size() == 0) {
+                countValueMap.remove(minCount);
+            }
+        }
+        keyValueMap.put(key, value);
+        int count = keyCountMap.getOrDefault(key, 0) + 1;
+        keyCountMap.put(key, count);
+        countValueMap.putIfAbsent(count, new ArrayList<>());
+        countValueMap.get(count).add(key);
     }
 
     /**
