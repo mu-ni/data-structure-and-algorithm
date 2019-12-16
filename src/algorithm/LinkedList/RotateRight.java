4ae2a4318deaa994f320fa71e551f7c26f306017
muni <muni@4paradigm.com>
2019-12-16 19:30:21 +0800

    add

--- src/algorithm/LinkedList/RotateRight.java
+++ src/algorithm/LinkedList/RotateRight.java
@@ -5,8 +5,8 @@ import algorithm.LinkedList.Dao.ListNode;
 public class RotateRight {
     public static void main(String[] args) {
         System.out.println(rotateRight(ListNode.getList(new int[]{1,2,3,4,5}), 2).toString());
-        System.out.println(rotateRight(ListNode.getList(new int[]{0,1,2}), 4).toString());
         System.out.println(rotateRight2(ListNode.getList(new int[]{1,2,3,4,5}), 2).toString());
+        System.out.println(rotateRight3(ListNode.getList(new int[]{1,2,3,4,5}), 2).toString());
     }
 
     public static ListNode rotateRight(ListNode head, int k) {
@@ -55,5 +55,33 @@ public class RotateRight {
         head = slow.next;
         slow.next = null;
         return head;
+    }
+
+    public static ListNode rotateRight3(ListNode head, int k) {
+        if (head == null) return null;
+        int len = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            len++;
+            cur = cur.next;
+        }
+        k %= len;
+        // no need to rotate
+        if (k == 0) return head;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i=0; i<k; i++) {
+            fast = fast.next;
+        }
+        for (int i=k; i<len-1; i++) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        ListNode newHead = slow.next;
+        slow.next = null;
+        fast.next = head;
+        return newHead;
     }
 }
