ef99d8ca377cf349bb28ae9b3728845db33ea225
muni <muni@4paradigm.com>
2019-12-17 22:12:50 +0800

    add

--- src/algorithm/Tree/SumRootToLeafNumbers.java
+++ src/algorithm/Tree/SumRootToLeafNumbers.java
@@ -9,6 +9,7 @@ public class SumRootToLeafNumbers {
     public static void main(String[] args) {
         System.out.println(new SumRootToLeafNumbers().sumNumbers(TreeNode.genTree()));
         System.out.println(new SumRootToLeafNumbers().sumNumbers2(TreeNode.genTree()));
+        System.out.println(new SumRootToLeafNumbers().sumNumbers3(TreeNode.genTree()));
     }
 
     int max = 0;
@@ -40,5 +41,22 @@ public class SumRootToLeafNumbers {
             return sum;
         }
         return helper(node.left, sum) + helper(node.right, sum);
+    }
+
+    public int sumNumbers3(TreeNode root) {
+        if (root == null) return 0;
+        return helper3(root, 0);
+    }
+
+    public int helper3(TreeNode node, int sum) {
+        sum = sum*10 + node.val;
+        if (node.left == null && node.right == null) {
+            return sum;
+        }
+
+        int rst = 0;
+        if (node.left != null) rst += helper3(node.left, sum);
+        if (node.right != null) rst += helper3(node.right, sum);
+        return rst;
     }
 }
