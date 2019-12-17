1bcd2cdab87a5ca59a29feefd2975660bd681b1f
muni <muni@4paradigm.com>
2019-12-17 20:55:06 +0800

    add

--- src/algorithm/ArrayAndString/Stock/MultipleTransactions.java
+++ src/algorithm/ArrayAndString/Stock/MultipleTransactions.java
@@ -3,6 +3,7 @@ package algorithm.ArrayAndString.Stock;
 public class MultipleTransactions {
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit2(new int[]{7,1,5,3,6,4}));
     }
 
     // multiple transactions
@@ -12,5 +13,16 @@ public class MultipleTransactions {
             sum += Math.max(prices[i]-prices[i-1], 0);
         }
         return sum;
+    }
+
+    public static int maxProfit2(int[] prices) {
+        if (prices.length < 2) return 0;
+        int max = 0;
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                max += prices[i] - prices[i-1];
+            }
+        }
+        return max;
     }
 }
