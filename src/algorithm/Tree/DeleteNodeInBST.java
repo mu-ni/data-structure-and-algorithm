5065bf42b9c0bf5d7c021edd1fcd16d25a5143fd
muni <muni@4paradigm.com>
2019-12-23 12:58:55 +0800

    add

--- src/algorithm/Tree/DeleteNodeInBST.java
+++ src/algorithm/Tree/DeleteNodeInBST.java
@@ -7,6 +7,9 @@ public class DeleteNodeInBST {
         TreeNode root = TreeNode.arr2tree(new Integer[]{5, 3, 6, 2, 4, null, 7});
         new DeleteNodeInBST().deleteNode(root, 3);
         System.out.println(TreeNode.tree2arr(root));
+        root = TreeNode.arr2tree(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        new DeleteNodeInBST().deleteNode2(root, 3);
+        System.out.println(TreeNode.tree2arr(root));
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
@@ -29,6 +32,33 @@ public class DeleteNodeInBST {
             root.val = leftMax.val;
             root.left = deleteNode(root.left, leftMax.val);
         }
+        return root;
+    }
+
+    public TreeNode deleteNode2(TreeNode root, int key) {
+        if (root == null) return null;
+        if (key < root.val) {
+            root.left = deleteNode2(root.left, key);
+            return root;
+        }
+        if (key > root.val) {
+            root.right = deleteNode2(root.right, key);
+            return root;
+        }
+        // key == root.val
+        if (root.left == null) {
+            return root.right;
+        }
+        if (root.right == null) {
+            return root.left;
+        }
+        TreeNode leftMax = root.left;
+        while (leftMax.right != null) {
+            leftMax = leftMax.right;
+        }
+        root.val = leftMax.val;
+        root.left = deleteNode2(root.left, leftMax.val);
+
         return root;
     }
 }
