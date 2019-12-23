1029b6f217315e8b218a4e62b332a2607db513cb
muni <muni@4paradigm.com>
2019-12-23 13:33:25 +0800

    add

--- src/algorithm/Tree/FindLargestValueInEachTreeRow.java
+++ src/algorithm/Tree/FindLargestValueInEachTreeRow.java
@@ -35,16 +35,17 @@ public class FindLargestValueInEachTreeRow {
     public List<Integer> largestValues2(TreeNode root) {
         if (root == null) return new ArrayList<>();
         List<Integer> rst = new ArrayList<>();
-        dfs(rst, root, 1);
+        dfs(rst, root, 0);
         return rst;
     }
 
     public void dfs(List<Integer> rst, TreeNode node, int level) {
         if (node == null) return;
-        if (rst.size() < level) {
+        if (rst.size() == level) {
             rst.add(node.val);
         } else {
-            rst.set(level-1, Math.max(node.val, rst.get(level-1)));
+            int max = Math.max(node.val, rst.get(level));
+            rst.set(level, max);
         }
 
         dfs(rst, node.left, level+1);
