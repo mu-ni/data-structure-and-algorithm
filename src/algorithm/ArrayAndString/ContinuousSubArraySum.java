9d729b9a53d6fe92d9000935afab671533a023f5
muni <muni@4paradigm.com>
2019-12-23 18:32:53 +0800

    add

--- src/algorithm/ArrayAndString/ContinuousSubArraySum.java
+++ src/algorithm/ArrayAndString/ContinuousSubArraySum.java
@@ -1,10 +1,15 @@
 package algorithm.ArrayAndString;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContinuousSubArraySum {
     public static void main(String[] args) {
         System.out.println(new ContinuousSubArraySum().checkSubarraySum(new int[]{23, 2, 6, 4, 7}, 6));
+        System.out.println(new ContinuousSubArraySum().checkSubarraySum2(new int[]{23, 2, 6, 4, 7}, 6));
     }
 
+    // at least 2 items in subarray
     public boolean checkSubarraySum(int[] nums, int k) {
         for (int i=0; i<nums.length; i++) {
             int sum =nums[i];
@@ -12,6 +17,22 @@ public class ContinuousSubArraySum {
                 sum += nums[j];
                 if ((k == 0 && sum == 0) || (k != 0 && sum % k == 0)) return true;
             }
+        }
+        return false;
+    }
+
+    public boolean checkSubarraySum2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i=0;i<nums.length;i++) {
+            sum += nums[i];
+            if (k != 0) sum %= k;
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+                continue;
+            }
+            if (i - map.get(sum) > 1) return true;
         }
         return false;
     }
--- src/algorithm/UnionFind/MinDistanceOfZero.java
+++ src/algorithm/UnionFind/MinDistanceOfZero.java
@@ -8,6 +8,8 @@ public class MinDistanceOfZero {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{0,0,0},{0,1,0},{1,1,1}};
         System.out.println(Arrays.deepToString(new MinDistanceOfZero().updateMatrix(matrix)));
+        matrix = new int[][]{{0,0,0},{0,1,0},{1,1,1}};
+        System.out.println(Arrays.deepToString(new MinDistanceOfZero().updateMatrix2(matrix)));
     }
 
     int m;
@@ -46,6 +48,44 @@ public class MinDistanceOfZero {
             }
             dist++;
         }
+        return -1;
+    }
+
+    public int[][] updateMatrix2(int[][] matrix) {
+        m = matrix.length;
+        if (m == 0) return matrix;
+        n = matrix[0].length;
+        if (n == 0) return matrix;
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (matrix[i][j] == 0) continue;
+                matrix[i][j] = bfs(matrix, i, j);
+            }
+        }
+        return matrix;
+    }
+
+    public int bfs(int[][] matrix, int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x,y});
+
+        int dist = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                int[] pos = queue.poll();
+                for (int[] dir : dirs) {
+                    int dx = pos[0] + dir[0];
+                    int dy = pos[1] + dir[1];
+                    if (dx < 0 || dy < 0 || dx >= m || dy >= n) continue;
+                    if (matrix[dx][dy] == 0) return dist;
+                    queue.offer(new int[]{dx,dy});
+                }
+            }
+            dist++;
+        }
+
         return -1;
     }
 }
