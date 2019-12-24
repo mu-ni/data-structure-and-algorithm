fea2f42d5321dbec37769a367b18ff8f309c7c36
muni <muni@4paradigm.com>
2019-12-24 18:15:41 +0800

    add

--- src/algorithm/Graph/NetworkDelayTime.java
+++ src/algorithm/Graph/NetworkDelayTime.java
@@ -6,6 +6,7 @@ public class NetworkDelayTime {
     public static void main(String[] args) {
         int[][] times = new int[][]{{2,1,1},{2,3,1},{3,4,1}};
         System.out.println(new NetworkDelayTime().networkDelayTime(times, 4, 2));
+        System.out.println(new NetworkDelayTime().networkDelayTime2(times, 4, 2));
     }
 
     // djikstra
@@ -36,5 +37,32 @@ public class NetworkDelayTime {
         }
 
         return -1;
+    }
+
+    public int networkDelayTime2(int[][] times, int N, int K) {
+        List<Map<Integer, Integer>> graph = new ArrayList<>();
+        for (int i=0; i<=N; i++) { // index from 1
+            graph.add(new HashMap<>());
+        }
+        for (int[] time : times) {
+            graph.get(time[0]).put(time[1], time[2]);
+        }
+
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        queue.offer(new int[]{K, 0});
+        boolean[] visited = new boolean[N+1];
+        int dist = 0;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int node = cur[0];
+            if (visited[node]) continue;
+            visited[node] = true;
+            dist = cur[1];
+            N--;
+            for (int next : graph.get(node).keySet()) {
+                queue.offer(new int[]{next, dist + graph.get(node).get(next)});
+            }
+        }
+        return N == 0 ? dist : -1;
     }
 }
