1ec6055c493c523b236271c5272a43ee66f1e52f
muni <muni@4paradigm.com>
2019-12-25 13:22:43 +0800

    add

--- src/algorithm/SearchAndSort/VerifyingAlienDictionary.java
+++ src/algorithm/SearchAndSort/VerifyingAlienDictionary.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 public class VerifyingAlienDictionary {
     public static void main(String[] args) {
         System.out.println(new VerifyingAlienDictionary().isAlienSorted(new String[]{"word","world","row"}, "worldabcefghijkmnpqstuvxyz"));
+        System.out.println(new VerifyingAlienDictionary().isAlienSorted2(new String[]{"word","world","row"}, "worldabcefghijkmnpqstuvxyz"));
     }
 
     public boolean isAlienSorted(String[] words, String order) {
@@ -21,6 +22,23 @@ public class VerifyingAlienDictionary {
         };
         String[] sorted = words.clone();
         Arrays.sort(sorted, comparator);
+        return Arrays.equals(words, sorted);
+    }
+
+    public boolean isAlienSorted2(String[] words, String order) {
+        String[] sorted = words.clone();
+        Arrays.sort(sorted, (a, b) -> {
+            int i1 = 0;
+            int i2 = 0;
+            while (i1 < a.length() && i2 < b.length()) {
+                Integer idx1 = order.indexOf(a.charAt(i1));
+                Integer idx2 = order.indexOf(b.charAt(i2));
+                if (!idx1.equals(idx2)) return idx1.compareTo(idx2);
+                i1++;
+                i2++;
+            }
+            return i1 < a.length() ? 1 : -1;
+        });
         return Arrays.equals(words, sorted);
     }
 }
