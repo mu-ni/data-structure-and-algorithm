0486b30a729c4f4744b0a9f4a36ff4501cf699f3
muni <efrei.muni@gmail.com>
2019-12-26 00:05:19 +0800

    add

--- src/algorithm/RecursionAndDP/MaxSubarraySumOneDeletion.java
+++ src/algorithm/RecursionAndDP/MaxSubarraySumOneDeletion.java
@@ -5,31 +5,30 @@ package algorithm.RecursionAndDP;
  */
 public class MaxSubarraySumOneDeletion {
     public static void main(String[] args) {
-        System.out.println(new MaxSubarraySumOneDeletion().maximumSum(new int[]{2,1,-2,-5,-2}));
+        System.out.println(new MaxSubarraySumOneDeletion().maximumSum(new int[]{1,-4,-5,-2,5,0,-1,2}));
     }
 
     public int maximumSum(int[] arr) {
         int n = arr.length;
-        if (n == 0) return 0;
-
+        if (n == 0) return -1;
         int rst = arr[0];
-        int[] maxEndHere = new int[n];
-        maxEndHere[0] = arr[0];
-        for (int i=1; i<n; i++) {
-            maxEndHere[i] = Math.max(arr[i], maxEndHere[i-1] + arr[i]);
-            rst = Math.max(rst, maxEndHere[i]);
-        }
 
-        int[] maxStartHere = new int[n];
-        maxStartHere[n-1] = arr[n-1];
+        int[] left = new int[n];
+        left[0] = arr[0];
+        for (int i=1; i<n; i++) {
+            left[i] = Math.max(arr[i], arr[i] + left[i-1]);
+            rst = Math.max(rst, left[i]);
+        }
+        int[] right = new int[n];
+        right[n-1] = arr[n-1];
         for (int i=n-2; i>=0; i--) {
-            maxStartHere[i] = Math.max(arr[i], maxStartHere[i+1] + arr[i]);
+            right[i] = Math.max(arr[i], arr[i] + right[i+1]);
             // not necessary
-//            rst = Math.max(rst, maxStartHere[i]);
+            rst = Math.max(rst, right[i]);
         }
 
         for (int i=1; i<n-1; i++) {
-            rst = Math.max(rst, maxStartHere[i+1] + maxEndHere[i-1]);
+            rst = Math.max(rst, left[i-1] + right[i+1]);
         }
         return rst;
     }
