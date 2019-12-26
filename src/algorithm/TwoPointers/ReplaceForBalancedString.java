08cc808696a8a5516baf075493b00036a41fb5e5
muni <muni@4paradigm.com>
2019-12-26 21:32:07 +0800

    add

--- src/algorithm/TwoPointers/ReplaceForBalancedString.java
+++ src/algorithm/TwoPointers/ReplaceForBalancedString.java
@@ -2,7 +2,7 @@ package algorithm.TwoPointers;
 
 public class ReplaceForBalancedString {
     public static void main(String[] args) {
-        System.out.println(new ReplaceForBalancedString().balancedString("QWER"));
+        System.out.println(new ReplaceForBalancedString().balancedString("WWEQERQWQWWRWWERQWEQ"));
     }
 
     public int balancedString(String s) {
@@ -14,10 +14,10 @@ public class ReplaceForBalancedString {
         int slow = 0;
         int fast = 0;
         int rst = s.length();
-        while (fast < s.length()) {
+        while (slow < s.length() && fast < s.length()) {
             arr[s.charAt(fast)]--;
             fast++;
-            while (slow < s.length() && arr['Q'] <= k && arr['W'] <= k && arr['E'] <= k && arr['R'] <= k) {
+            while (arr['Q'] <= k && arr['W'] <= k && arr['E'] <= k && arr['R'] <= k) {
                 rst = Math.min(rst, fast - slow);
                 arr[s.charAt(slow)]++;
                 slow++;
@@ -25,4 +25,27 @@ public class ReplaceForBalancedString {
         }
         return rst;
     }
+
+//    // WRONG -> minimum length of [substring] -> substring, not character
+//    public int balancedString2(String s) {
+//        int count = s.length()/4;
+//        int[] arr = new int[4];
+//        for (char c : s.toCharArray()) {
+//            if (c == 'Q') {
+//                arr[0]++;
+//            } else if (c == 'W') {
+//                arr[1]++;
+//            } else if (c == 'E') {
+//                arr[2]++;
+//            } else {
+//                arr[3]++;
+//            }
+//        }
+//
+//        int rst = 0;
+//        for (int n : arr) {
+//            rst += Math.abs(count - n);
+//        }
+//        return rst/2;
+//    }
 }
