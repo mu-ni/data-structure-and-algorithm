282c61543e1f5d9712f4674c3d0d386e76ce2925
muni <muni@4paradigm.com>
2019-12-26 14:35:17 +0800

    add

--- src/algorithm/TwoPointers/GetEqualSubstringWithinBudget.java
+++ src/algorithm/TwoPointers/GetEqualSubstringWithinBudget.java
@@ -3,6 +3,7 @@ package algorithm.TwoPointers;
 public class GetEqualSubstringWithinBudget {
     public static void main(String[] args) {
         System.out.println(new GetEqualSubstringWithinBudget().equalSubstring("abcd", "bcdf", 3));
+        System.out.println(new GetEqualSubstringWithinBudget().equalSubstring2("abcd", "bcdf", 3));
     }
 
     public int equalSubstring(String s, String t, int maxCost) {
@@ -21,5 +22,23 @@ public class GetEqualSubstringWithinBudget {
             maxLen = Math.max(maxLen, fast - slow);
         }
         return maxLen;
+    }
+
+    public int equalSubstring2(String s, String t, int maxCost) {
+        int slow = 0;
+        int fast = 0;
+        int cost = 0;
+        int rst = 0;
+        while (fast < s.length()) {
+            cost += Math.abs(s.charAt(fast) - t.charAt(fast));
+            fast++;
+            if (cost <= maxCost) {
+                rst = Math.max(rst, fast - slow);
+            } else {
+                cost -= Math.abs(s.charAt(slow) - t.charAt(slow));
+                slow++;
+            }
+        }
+        return rst;
     }
 }
