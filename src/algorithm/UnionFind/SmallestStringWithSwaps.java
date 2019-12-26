9f4b258c562b18177d6f44a8feea54649c37e82d
muni <muni@4paradigm.com>
2019-12-26 14:20:49 +0800

    add

--- src/algorithm/UnionFind/SmallestStringWithSwaps.java
+++ src/algorithm/UnionFind/SmallestStringWithSwaps.java
@@ -9,19 +9,12 @@ public class SmallestStringWithSwaps {
         List<List<Integer>> pairs = Arrays.stream(arr).map(Arrays::asList).collect(Collectors.toList());
         System.out.println(new SmallestStringWithSwaps().smallestStringWithSwaps("dcab", pairs));
         System.out.println(new SmallestStringWithSwaps().smallestStringWithSwaps2("dcab", pairs));
+        System.out.println(new SmallestStringWithSwaps().smallestStringWithSwaps3("dcab", pairs));
     }
 
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (List<Integer> pair : pairs) {
-            List<Integer> list = graph.getOrDefault(pair.get(0), new ArrayList<>());
-            list.add(pair.get(1));
-            graph.put(pair.get(0), list);
-
-            list = graph.getOrDefault(pair.get(1), new ArrayList<>());
-            list.add(pair.get(0));
-            graph.put(pair.get(1), list);
-        }
+        buildGraph(graph, pairs);
 
         boolean[] visited = new boolean[s.length()];
         char[] arr = s.toCharArray();
@@ -37,24 +30,74 @@ public class SmallestStringWithSwaps {
                 visited[cur] = true;
                 lc.add(s.charAt(cur));
                 li.add(cur);
-                List<Integer> nextList = graph.get(cur);
-                if (nextList != null) {
-                    for (int next : nextList) {
-                        queue.offer(next);
-                    }
+                for (int next : graph.getOrDefault(cur, new ArrayList<>())) {
+                    queue.offer(next);
                 }
             }
-            Collections.sort(lc);
-            Collections.sort(li);
-            for (int j=0; j<lc.size(); j++) {
-                arr[li.get(j)] = lc.get(j);
-            }
+            buildArr(arr, lc, li);
         }
 
         return new String(arr);
     }
 
+    private void buildArr(char[] arr, List<Character> lc, List<Integer> li) {
+        Collections.sort(lc);
+        Collections.sort(li);
+        for (int j=0; j<lc.size(); j++) {
+            arr[li.get(j)] = lc.get(j);
+        }
+    }
+
     public String smallestStringWithSwaps2(String s, List<List<Integer>> pairs) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        buildGraph(graph, pairs);
+
+        char[] arr = s.toCharArray();
+        boolean[] visited = new boolean[s.length()];
+        for (int i=0; i<s.length(); i++) {
+            if (visited[i]) continue;
+            List<Integer> list = new ArrayList<>();
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(i);
+            while (!queue.isEmpty()) {
+                int cur = queue.poll();
+                if (visited[cur]) continue;
+                visited[cur] = true;
+                list.add(cur);
+                for (int next : graph.getOrDefault(cur, new ArrayList<>())) {
+                    queue.offer(next);
+                }
+            }
+            buildArr(arr, s, list);
+        }
+        return new String(arr);
+    }
+
+    private void buildArr(char[] arr, String s, List<Integer> list) {
+        char[] sorted = new char[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            sorted[i] = s.charAt(list.get(i));
+        }
+        Arrays.sort(sorted);
+        Collections.sort(list);
+        for (int i=0; i<list.size(); i++) {
+            arr[list.get(i)] = sorted[i];
+        }
+    }
+
+    private void buildGraph(Map<Integer, List<Integer>> graph, List<List<Integer>> pairs) {
+        for (List<Integer> pair : pairs) {
+            List<Integer> list = graph.getOrDefault(pair.get(0), new ArrayList<>());
+            list.add(pair.get(1));
+            graph.put(pair.get(0), list);
+
+            list = graph.getOrDefault(pair.get(1), new ArrayList<>());
+            list.add(pair.get(0));
+            graph.put(pair.get(1), list);
+        }
+    }
+
+    public String smallestStringWithSwaps3(String s, List<List<Integer>> pairs) {
         int[] p = new int[s.length()];
         for (int i = 0; i < p.length; i++) p[i] = i;
         for (List<Integer> pair : pairs) {
