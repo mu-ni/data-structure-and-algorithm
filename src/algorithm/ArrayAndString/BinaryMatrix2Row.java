2521d0e276feee0ee8eccb87404d0ac1c615cc04
muni <muni@4paradigm.com>
2019-12-27 14:53:57 +0800

    add

--- src/algorithm/ArrayAndString/BinaryMatrix2Row.java
+++ src/algorithm/ArrayAndString/BinaryMatrix2Row.java
@@ -1,17 +1,22 @@
 package algorithm.ArrayAndString;
 
+import algorithm.LinkedList.Dao.ListNode;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BinaryMatrix2Row {
     public static void main(String[] args) {
         System.out.println(new BinaryMatrix2Row().reconstructMatrix(5, 5, new int[]{2,1,3,0,1,0,1,1,0,1}));
         System.out.println(new BinaryMatrix2Row().reconstructMatrix2(5, 5, new int[]{2,1,3,0,1,0,1,1,0,1}));
+        System.out.println(new BinaryMatrix2Row().reconstructMatrix3(5, 5, new int[]{2,1,3,0,1,0,1,1,0,1}));
     }
 
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
-        if (upper + lower != sum(colsum)) return new ArrayList<>();
+        if (upper + lower != Arrays.stream(colsum).sum()) return new ArrayList<>();
 
         List<List<Integer>> rst = new ArrayList<>();
         rst.add(getLine(upper, colsum));
@@ -39,7 +44,7 @@ public class BinaryMatrix2Row {
     }
 
     public List<List<Integer>> reconstructMatrix2(int upper, int lower, int[] colsum) {
-        if (upper + lower != sum(colsum)) return new ArrayList<>();
+        if (upper + lower != Arrays.stream(colsum).sum()) return new ArrayList<>();
 
         List<List<Integer>> rst = new ArrayList<>();
         rst.add(getLine2(upper, colsum));
@@ -67,11 +72,33 @@ public class BinaryMatrix2Row {
         return line;
     }
 
-    public int sum(int[] arr) {
+    public List<List<Integer>> reconstructMatrix3(int upper, int lower, int[] colsum) {
+        if (upper + lower != Arrays.stream(colsum).sum()) return new ArrayList<>();
+
+        List<Integer> line1 = Arrays.stream(getLine3(upper, colsum)).boxed().collect(Collectors.toList());
+        List<Integer> line2 = Arrays.stream(getLine3(lower, colsum)).boxed().collect(Collectors.toList());
+        List<List<Integer>> rst = new ArrayList<>();
+        rst.add(line1);
+        rst.add(line2);
+        return rst;
+    }
+
+    public int[] getLine3(int val, int[] colsum) {
+        int n = colsum.length;
+        int[] arr = new int[n];
         int sum = 0;
-        for (int n : arr) {
-            sum += n;
+        for (int j=0; j<n; j++) {
+            if (colsum[j] == 0) continue;
+            if (sum + colsum[j] <= val) {
+                arr[j] = colsum[j];
+                sum += colsum[j];
+                colsum[j] = 0;
+            } else {
+                arr[j] = val - sum;
+                colsum[j] = colsum[j] - arr[j];
+                return arr;
+            }
         }
-        return sum;
+        return arr;
     }
 }
