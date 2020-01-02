afcec2913dddfa36aebe90fb22cffeec7a916a7e
muni <muni@4paradigm.com>
2020-01-02 17:22:34 +0800

    add

--- src/algorithm/TwoPointers/ReplaceForBalancedString.java
+++ src/algorithm/TwoPointers/ReplaceForBalancedString.java
@@ -3,6 +3,7 @@ package algorithm.TwoPointers;
 public class ReplaceForBalancedString {
     public static void main(String[] args) {
         System.out.println(new ReplaceForBalancedString().balancedString("WWEQERQWQWWRWWERQWEQ"));
+        System.out.println(new ReplaceForBalancedString().balancedString2("WWEQERQWQWWRWWERQWEQ"));
     }
 
     public int balancedString(String s) {
@@ -19,6 +20,7 @@ public class ReplaceForBalancedString {
             fast++;
             // move slow -> s(0, slow) + s(fast, n) is balanced
             // arr -> chars in s(0, slow) + s(fast, n)
+            // each character appears n/4 times
             while (arr['Q'] <= k && arr['W'] <= k && arr['E'] <= k && arr['R'] <= k) {
                 rst = Math.min(rst, fast - slow);
                 arr[s.charAt(slow)]++;
@@ -26,6 +28,29 @@ public class ReplaceForBalancedString {
             }
         }
         return rst;
+    }
+
+    public int balancedString2(String s) {
+        int[] arr = new int[128];
+        int k = s.length()/4;
+        for (char c : s.toCharArray()) {
+            arr[c]++;
+        }
+        if(arr['Q'] == k && arr['W'] == k && arr['E'] == k && arr['R'] == k) return 0;
+
+        int slow = 0;
+        int fast = 0;
+        int min = Integer.MAX_VALUE;
+        while (fast < s.length()) {
+            arr[s.charAt(fast)]--;
+            fast++;
+            while (arr['Q'] <= k && arr['W'] <= k && arr['E'] <= k && arr['R'] <= k) {
+                min = Math.min(min, fast-slow);
+                arr[s.charAt(slow)]++;
+                slow++;
+            }
+        }
+        return min;
     }
 
 //    // WRONG -> minimum length of [substring] -> substring, not character
