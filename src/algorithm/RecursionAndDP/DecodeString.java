e53bc8afd9372099733e12169b2bf24bf18a198c
muni <muni@4paradigm.com>
2020-01-03 14:14:18 +0800

    add

--- src/algorithm/RecursionAndDP/DecodeString.java
+++ src/algorithm/RecursionAndDP/DecodeString.java
@@ -7,7 +7,6 @@ public class DecodeString {
         System.out.println(new DecodeString().decodeString0("2[abc]3[cd]ef"));
         System.out.println(new DecodeString().decodeString("3[2[xyz]4[ab]]ef"));
         System.out.println(new DecodeString().decodeString2("3[2[xyz]4[ab]]ef"));
-        System.out.println(new DecodeString().decodeString3("3[2[xyz]4[ab]]ef"));
     }
 
     // no loop
@@ -32,12 +31,11 @@ public class DecodeString {
     }
 
     public String decodeString(String s) {
-        Stack<Integer> istack = new Stack<>();
-        Stack<String> stack = new Stack<>();
-        int n = 0;
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
+        Stack<String> stack = new Stack<>();
+        Stack<Integer> istack = new Stack<>();
+        int n = 0;
+        for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) {
                 n = n*10 + c - '0';
             } else if (c == '[') {
@@ -46,14 +44,12 @@ public class DecodeString {
                 stack.push(sb.toString());
                 sb = new StringBuilder();
             } else if (c == ']') {
-                String str = stack.pop();
-                str += repeat(sb.toString(), istack.pop());
+                String str = stack.pop() + repeat(sb.toString(), istack.pop());
                 sb = new StringBuilder(str);
             } else {
                 sb.append(c);
             }
         }
-
         return sb.toString();
     }
 
@@ -84,30 +80,6 @@ public class DecodeString {
                 sb.append(repeat(tmp.toString(), s1.pop()));
             } else {
                 sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
-
-    public String decodeString3(String s) {
-        Stack<Integer> s1 = new Stack<>();
-        Stack<String> s2 = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        int n = 0;
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                n = n*10 + c - '0';
-            } else if (Character.isLetter(c)) {
-                sb.append(c);
-            } else if (c == '[') {
-                s1.push(n);
-                n = 0;
-                s2.push(sb.toString());
-                sb = new StringBuilder();
-            } else if (c == ']') {
-                String str = s2.pop();
-                str += repeat(sb.toString(), s1.pop());
-                sb = new StringBuilder(str);
             }
         }
         return sb.toString();
