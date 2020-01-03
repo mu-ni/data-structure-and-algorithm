7f384288c072fe6143e87ceaf69a3dc50c5235e1
muni <muni@4paradigm.com>
2020-01-03 17:32:41 +0800

    add

--- src/algorithm/Tree/TrimBinarySearchTree.java
+++ src/algorithm/Tree/TrimBinarySearchTree.java
@@ -8,6 +8,10 @@ public class TrimBinarySearchTree {
         TreeNode root = TreeNode.arr2tree(new Integer[]{3,0,4,null,2,null,null,null,null,1});
         TreeNode tree = new TrimBinarySearchTree().trimBST(root, 1, 3);
         System.out.println(new PreOrder().preOrder(tree));
+
+        root = TreeNode.arr2tree(new Integer[]{3,0,4,null,2,null,null,null,null,1});
+        tree = new TrimBinarySearchTree().trimBST2(root, 1, 3);
+        System.out.println(new PreOrder().preOrder(tree));
     }
 
     public TreeNode trimBST(TreeNode root, int L, int R) {
@@ -17,6 +21,16 @@ public class TrimBinarySearchTree {
 
         root.left = trimBST(root.left, L, R);
         root.right = trimBST(root.right, L, R);
+        return root;
+    }
+
+    public TreeNode trimBST2(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        if (root.val < L) return trimBST2(root.right, L, R);
+        if (root.val > R) return trimBST2(root.left, L, R);
+
+        root.left = trimBST2(root.left, L, root.val);
+        root.right = trimBST2(root.right, root.val, R);
         return root;
     }
 }
