faba92df1983996b5293a71b7a4c70072f6e3030
muni <muni@4paradigm.com>
2020-01-06 15:14:06 +0800

    add

--- src/algorithm/SearchAndSort/SearchRange.java
+++ src/algorithm/SearchAndSort/SearchRange.java
@@ -80,45 +80,40 @@ public class SearchRange {
     }
 
     public int[] searchRange3(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        // find left position
-        int leftPos = -1;
-        while (left <= right) {
-            int mid = left + (right-left)/2;
-            if (nums[mid] > target) {
-                right = mid-1;
-            } else if (nums[mid] < target) {
-                left = mid+1;
+        int start = 0;
+        int end = nums.length-1;
+        int[] rst = new int[]{-1, -1};
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (nums[mid] < target) {
+                start = mid+1;
+            } else if (nums[mid] > target) {
+                end = mid-1;
             } else {
                 if (mid == 0 || nums[mid-1] < target) {
-                    leftPos = mid;
+                    rst[0] = mid;
                     break;
                 }
-                right = mid-1;
+                end = mid-1;
             }
         }
-
-        if (leftPos == -1) return new int[]{-1, -1};
-
-        left = leftPos;
-        right = nums.length-1;
-        // find right position
-        int rightPos = -1;
-        while (left <= right) {
-            int mid = left + (right-left)/2;
+        start = 0;
+        end = nums.length-1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
             if (nums[mid] > target) {
-                right = mid-1;
+                end = mid-1;
             } else if (nums[mid] < target) {
-                left = mid+1;
+                start = mid+1;
             } else {
                 if (mid == nums.length-1 || nums[mid+1] > target) {
-                    rightPos = mid;
+                    rst[1] = mid;
                     break;
                 }
-                left = mid+1;
+                start = mid+1;
             }
         }
-        return new int[]{leftPos, rightPos};
+
+        return rst;
     }
 }
