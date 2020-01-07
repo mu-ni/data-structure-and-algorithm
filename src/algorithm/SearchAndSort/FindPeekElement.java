9c5a6d3daac01dcdf6c35c0a617882c8c3cb0925
muni <muni@4paradigm.com>
2020-01-07 15:01:03 +0800

    add

--- src/algorithm/SearchAndSort/FindPeekElement.java
+++ src/algorithm/SearchAndSort/FindPeekElement.java
@@ -3,6 +3,8 @@ package algorithm.SearchAndSort;
 public class FindPeekElement {
     public static void main(String[] args) {
         System.out.println(new FindPeekElement().findPeakElement(new int[]{1,2,1,3,5,6,4}));
+        System.out.println(new FindPeekElement().findPeakElement2(new int[]{1,2,1,3,5,6,4}));
+        System.out.println(new FindPeekElement().findPeakElement3(new int[]{1,2,1,3,5,6,4}));
         // output 1 or 5
     }
 
@@ -17,6 +19,37 @@ public class FindPeekElement {
             } else {
                 if (mid == 0 || nums[mid] >= nums[mid-1]) return mid;
                 end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public int findPeakElement2(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (start == end) return mid;
+            if (mid != end && nums[mid] > nums[mid+1]) {
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    public int findPeakElement3(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (start == end) return mid;
+            if (mid != end && nums[mid] > nums[mid+1]) {
+                if (mid == start || nums[mid] > nums[mid-1]) return mid;
+                end = mid-1;
+            } else {
+                start = mid+1;
             }
         }
         return -1;
