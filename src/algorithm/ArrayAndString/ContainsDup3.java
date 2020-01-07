386f2755cee3dbe8bd3efd3e9e298af68d4d9d7a
muni <muni@4paradigm.com>
2020-01-07 18:13:48 +0800

    add

--- src/algorithm/ArrayAndString/ContainsDup3.java
+++ src/algorithm/ArrayAndString/ContainsDup3.java
@@ -36,4 +36,23 @@ public class ContainsDup3 {
         }
         return false;
     }
+
+//    // nums[i] and nums[j] <= t
+//    // i and j <= k
+//    public boolean containsNearbyAlmostDuplicate3(int[] nums, int k, int t) {
+//        TreeSet<Integer> set = new TreeSet<>();
+//        for (int i=0; i<nums.length; i++) {
+//            Integer ceiling = set.ceiling(nums[i]);
+//            // new int[]{-1,2147483647}, 1,2147483647) integer overflow
+//            if (ceiling != null && ceiling - nums[i] <= t) return true;
+//            Integer floor = set.floor(nums[i]);
+//            // new int[]{-1,2147483647}, 1,2147483647) integer overflow
+//            if (floor != null && nums[i] - floor <= t) return true;
+//            set.add(nums[i]);
+//            if (set.size() > k) {
+//                set.remove(nums[i-k]);
+//            }
+//        }
+//        return false;
+//    }
 }
