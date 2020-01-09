cd9bfa1915b7991b671764dd41b26665894d6482
muni <efrei.muni@gmail.com>
2020-01-09 20:57:50 +0800

    add

--- src/algorithm/Greedy/JumpGame2.java
+++ src/algorithm/Greedy/JumpGame2.java
@@ -3,7 +3,8 @@ package algorithm.Greedy;
 public class JumpGame2 {
     public static void main(String[] args) {
         System.out.println(new JumpGame2().jump(new int[]{2, 3, 1, 1, 4}));
-        System.out.println(new JumpGame2().jump2(new int[]{0}));
+        System.out.println(new JumpGame2().jump2(new int[]{2, 3, 1, 1, 4}));
+        System.out.println(new JumpGame2().jump3(new int[]{2, 3, 1, 1, 4}));
     }
 
     public int jump(int[] nums) {
@@ -34,5 +35,21 @@ public class JumpGame2 {
             }
         }
         return pos == 0 ? 0 : -1;
+    }
+
+    public int jump3(int[] nums) {
+        if (nums.length <= 1) return 0;
+        int pos = 0;
+        int max = 0;
+        int step = 0;
+        for (int i=0; i<nums.length; i++) {
+            max = Math.max(max, i+nums[i]);
+            if (pos == i) {
+                pos = max;
+                step++;
+                if (pos >= nums.length-1) return step;
+            }
+        }
+        return -1;
     }
 }
