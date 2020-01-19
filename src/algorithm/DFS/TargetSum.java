75c2f89395a8f31e507cbdabfc377d6609c5dd84
muni <muni@4paradigm.com>
2020-01-19 20:46:41 +0800

    add

--- src/algorithm/DFS/TargetSum.java
+++ src/algorithm/DFS/TargetSum.java
@@ -1,11 +1,14 @@
 package algorithm.DFS;
 
+import java.util.Arrays;
+
 /**
  * Created by muni on 2020/1/19
  */
 public class TargetSum {
     public static void main(String[] args) {
         System.out.println(new TargetSum().findTargetSumWays(new int[]{1,1,1,1,1}, 3));
+        System.out.println(new TargetSum().findTargetSumWays2(new int[]{1,1,1,1,1}, 3));
     }
 
     int count = 0;
@@ -25,5 +28,31 @@ public class TargetSum {
         }
         dfs(nums, i+1, sum + nums[i]);
         dfs(nums, i+1, sum - nums[i]);
+    }
+
+
+    // not really understand
+    public int findTargetSumWays2(int[] nums, int S) {
+        int sum = Arrays.stream(nums).sum();
+        if (S < -sum || S > sum) return 0;
+
+        int n = nums.length;
+        int[][] dp = new int[n+1][sum*2+1];
+        dp[0][sum] = 1;
+        int min = 0;
+        int max = sum*2;
+        for (int i=1; i<=n; i++) {
+            for (int j=min; j<=max; j++) {
+                int num = nums[i-1];
+                if (j + num <= max) {
+                    dp[i][j] += dp[i-1][j+num];
+                }
+                if (j - num >= min) {
+                    dp[i][j] += dp[i-1][j-num];
+                }
+            }
+        }
+
+        return dp[n][S + sum];
     }
 }
