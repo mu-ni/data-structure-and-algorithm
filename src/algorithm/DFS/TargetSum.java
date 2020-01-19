5fb848e24cf45a0da9a9558effb39d96596832c6
muni <muni@4paradigm.com>
2020-01-19 20:35:21 +0800

    add

---
+++ src/algorithm/DFS/TargetSum.java
@@ -0,0 +1,32 @@
+package algorithm.DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by muni on 2020/1/19
+ */
+public class TargetSum {
+    public static void main(String[] args) {
+        System.out.println(new TargetSum().findTargetSumWays(new int[]{1,1,1,1,1}, 3));
+    }
+
+    int count = 0;
+    int S = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        this.S = S;
+        dfs(nums, 0, 0);
+        return count;
+    }
+
+    private void dfs(int[] nums, int i, int sum) {
+        if (i == nums.length) {
+            if (sum == S) {
+                count++;
+            }
+            return;
+        }
+        dfs(nums, i+1, sum + nums[i]);
+        dfs(nums, i+1, sum - nums[i]);
+    }
+}
