94cae674f90bf44e43b38c5ec5f82bf83a8e9eea
muni <muni@4paradigm.com>
2020-07-03 17:32:17 +0800

    add

--- src/algorithm/ArrayAndString/PrisonAfterNDays.java
+++ src/algorithm/ArrayAndString/PrisonAfterNDays.java
@@ -11,19 +11,23 @@ public class PrisonAfterNDays {
         // [0,1,0,1,0,0,1,0]
         System.out.println(Arrays.toString(new PrisonAfterNDays().prisonAfterNDays(new int[]{0,1,1,1,0,0,0,0}, 99)));
         // [0,0,1,0,0,1,1,0]
+        System.out.println(Arrays.toString(new PrisonAfterNDays().prisonAfterNDays2(new int[]{1,0,0,1,0,0,1,0}, 100)));
+        // [0,1,0,1,0,0,1,0]
+        System.out.println(Arrays.toString(new PrisonAfterNDays().prisonAfterNDays2(new int[]{0,1,1,1,0,0,0,0}, 99)));
+        // [0,0,1,0,0,1,1,0]
     }
     public int[] prisonAfterNDays(int[] cells, int N) {
         Map<String, Integer> map = new HashMap<>();
         map.put(Arrays.toString(cells), 0);
         boolean hasCycle = false;
         int cycleLen = 0;
-        for (int i=1; i<=N; i++) {
+        for (int i=1; i<=N; i++) { // N times
             cells = next(cells);
             String key = Arrays.toString(cells);
             if (map.containsKey(key)) {
                 hasCycle = true;
                 cycleLen =  i - map.get(key);
-                N -= map.get(key);
+                N -= map.get(key); // remove prefix(not included in the cycle)
                 break;
             }
             map.put(key, i);
@@ -34,6 +38,20 @@ public class PrisonAfterNDays {
         while (N > 0) {
             cells = next(cells);
             N--;
+        }
+        return cells;
+    }
+
+    // not understand
+    public int[] prisonAfterNDays2(int[] cells, int N) {
+        Map<String, Integer> map = new HashMap<>();
+        while (N > 0) {
+            map.put(Arrays.toString(cells), N--);
+            cells = next(cells);
+            String key = Arrays.toString(cells);
+            if (map.containsKey(key)) {
+                N %= map.get(key) - N;
+            }
         }
         return cells;
     }
