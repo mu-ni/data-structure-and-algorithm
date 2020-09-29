f620dbc1e1346f92d23ff3a1647f4979c26e86a9
muni <muni@4paradigm.com>
2020-09-29 19:02:00 +0800

    add

---
+++ src/algorithm/Greedy/MinOperationsMaxProfit.java
@@ -0,0 +1,14 @@
+package algorithm.Greedy;
+
+/**
+ * Created by muni on 2020/9/29
+ */
+public class MinOperationsMaxProfit {
+    public static void main(String[] args) {
+        System.out.println(new MinOperationsMaxProfit().minOperationsMaxProfit(new int[]{10,9,6}, 6, 4));
+    }
+
+    public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
+        return -1;
+    }
+}
